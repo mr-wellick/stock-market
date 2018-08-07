75643f1fd6c57a12dcaeb01d08be5f56fe40b319
Pablo Lorenzo <lorenz3192@gmail.com>
2018-08-06 23:11:23 -0700

    changed Form.js component from class based to functional

--- src/js/Components/Form/Form.js
+++ src/js/Components/Form/Form.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
-import PropTypes            from "prop-types";
+import React     from "react";
+import PropTypes from "prop-types";
 import "./form.scss";
 
-class Form extends Component{
-    render(){
-        return(
-            <form onSubmit={ this.props.onSubmit } className="section__form">
-                <input type="text" id="section__form-input" placeholder="Enter a stock ticker"/>
-                <input type="submit" value="View" className="section__form-btn"/>
-            </form>
-        );
-    }
-}
+const Form = (props) => {
+    return(
+        <form onSubmit={ props.onSubmit } className="section__form">
+            <input type="text" id="section__form-input" placeholder="Enter a stock ticker"/>
+            <input type="submit" value="View" className="section__form-btn"/>
+        </form>
+    );
+};
 
 Form.propTypes = {
     onSubmit: PropTypes.func
