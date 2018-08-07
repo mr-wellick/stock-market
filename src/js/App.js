74f56779b49513764874ba43be1e1df068422947
Pablo Lorenzo <lorenz3192@gmail.com>
2018-08-06 20:42:26 -0700

    removed inline css style from Home.js component

--- src/js/App.js
+++ src/js/App.js
@@ -5,22 +5,13 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 // Import user defined components
 import { Home } from "./Views/Home";
 
-// for now use this css
-const headerCSS = {
-    display: "grid",
-    gridTemplateColumns: "1fr 1fr",
-    alignItems: "center",
-    justifyItems: "center",
-    margin: "10px"
-};
-
 // Application
 class App extends Component{
     render(){
         return(
             <Router>
                 <Fragment>
-                    <header style={ headerCSS }>
+                    <header>
                         <h1>LRNZ</h1>
                         <nav>
                             <ul>
