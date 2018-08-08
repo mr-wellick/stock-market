74bc31261e4bfcd41e2309c1960aa69d68489582
Pablo Lorenzo <lorenz3192@gmail.com>
2018-08-07 18:20:24 -0700

    added x and y axis to ScatterPlot.js

--- src/js/Components/ScatterPlot/ScatterPlot.js
+++ src/js/Components/ScatterPlot/ScatterPlot.js
@@ -1,22 +1,80 @@
 import React, { Component } from "react";
 import PropTypes            from "prop-types";
 import { select }           from "d3-selection";
+import { scaleLinear }      from "d3-scale";
+import { scaleTime }        from "d3-scale";
+import { max, min }         from "d3-array";
+import { axisBottom }       from "d3-axis";
+import { axisLeft }         from "d3-axis";
 import "./scatterPlot.scss";
+
+// Find xScale
+function findXScale(data, width = 500, padding = 10){
+
+    let lastEntry = data.length - 1;
+    let minDate   = new Date(data[0][0]);
+    let maxDate   = new Date(data[lastEntry][0]);
+    let scale     = scaleTime().domain([minDate, maxDate]);
+    scale.range([padding, width - padding]);
+
+    return(scale);
+}
+
+function findYScale(data, height = 460, padding = 10){
+
+
+    let yMin  = min(data, d => d[1]);
+    let yMax  = max(data, d => d[1]);
+    let scale = scaleLinear().domain([yMin, yMax]);
+    scale.range([height - padding, padding]);
+
+    return(scale);
+}
 
 class ScatterPlot extends Component{
 
     componentDidUpdate(){
+
+        let height  = 500;
+        let padding = 50;
+
         let { stockData } = this.props.data;
-        let adjustedClose = Object.entries(stockData).map( item => item[1]["5. adjusted close"] );
+        let doubleArray   = Object.entries(stockData).map(item => [item[0], Number(item[1]["5. adjusted close"])]);
+        doubleArray       = doubleArray.reverse();
 
         // Need to remove old nodes first. Then update. Currently,
         // all stock data remains present. We don't want this!
         select(this.node)
             .append("g")
             .selectAll("circle")
-            .data(adjustedClose)
+            .data(doubleArray)
             .enter()
             .append("circle");
+
+        // Find x and y scales
+        let x = findXScale(doubleArray);
+        let y = findYScale(doubleArray);
+
+        // Find x
+        let xAxis = axisBottom(x);
+        select(this.node)
+            .append("g")
+            .attr("transform", "translate(0," + (height - padding) + ")")
+            .call(xAxis);
+
+        // Find y
+        let yAxis = axisLeft(y);
+        select(this.node)
+            .append("g")
+            .attr("transform", "translate(" + padding + ",0)")
+            .call(yAxis);
+
+        // Finally add data points
+        //select(this.node)
+        //    .selectAll("circle")
+        //    .attr("cx", d => x(d[0]))
+        //    .attr("cy", d => y(d[1]))
+        //    .attr("r", 2);
     }
 
     render(){
@@ -24,7 +82,7 @@ class ScatterPlot extends Component{
             <svg
                 ref={ node => this.node = node }
                 width="500"
-                height="300"
+                height="500"
             />
         );
     }
