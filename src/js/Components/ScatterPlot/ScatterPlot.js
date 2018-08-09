3d1f54e3fbf6d5f88b90364705e427930a848fb2
Pablo Lorenzo <lorenz3192@gmail.com>
2018-08-08 21:25:41 -0700

    Finally got the ScatterPlot.js to work correctly

--- src/js/Components/ScatterPlot/ScatterPlot.js
+++ src/js/Components/ScatterPlot/ScatterPlot.js
@@ -1,27 +1,29 @@
 import React, { Component }       from "react";
 import PropTypes                  from "prop-types";
-import { findXScale, findYScale } from "./Utilities";
+//import { findXScale, findYScale } from "./Utilities";
 import { select }                 from "d3-selection";
 import { axisBottom }             from "d3-axis";
 import { axisLeft }               from "d3-axis";
 import { timeParse }              from "d3-time-format";
 import "./scatterPlot.scss";
+import { min, max } from "d3-array";
+import { scaleTime } from "d3-scale";
+import { scaleLinear } from "d3-scale";
+import { line } from "d3-shape";
 
-// Now, redo a line chart example in d3 to finalize this component.
+// Now, reformat data labels.
 class ScatterPlot extends Component{
     componentDidUpdate(){
 
-        let height        = 500;
+        // Get width, height, paddin
+        let { height }    = this.props;
+        let { width }     = this.props;
         let padding       = 50;
+
+        // Parse string dates to string objects, string values to numbers, and store data
         let parseTime     = timeParse("%Y-%m-%d");
         let { stockData } = this.props.data;
-        let doubleArray   = stockData.map(
-            item =>
-            [
-                parseTime(item[0]),
-                Number(item[1]["5. adjusted close"])
-            ]
-        );
+        let data          = stockData.map( item => [ parseTime(item[0]), Number(item[1]["5. adjusted close"]) ] );
 
         // Need to remove old nodes first then update.
         if(this.node.children.length > 0){
@@ -30,37 +32,42 @@ class ScatterPlot extends Component{
                 .remove();
         }
 
-        select(this.node)
-            .append("g")
-            .selectAll("circle")
-            .data(doubleArray)
-            .enter()
-            .append("circle");
+        // Find x-scale
+        let xMin   = min(data, d => d[0]);
+        let xMax   = max(data, d => d[0]);
+        let xScale = scaleTime().domain([xMin, xMax]);
+        xScale.range([padding, width - padding]);
 
-        // Find x and y scales
-        let x = findXScale(doubleArray);
-        let y = findYScale(doubleArray);
+        // Find y-scale
+        let yMin = min(data, d => d[1]);
+        let yMax = max(data, d => d[1]);
+        let yScale = scaleLinear().domain([yMin, yMax]);
+        yScale.range([height - padding, padding]);
 
-        // Find x and x-axis
-        let xAxis = axisBottom(x);
+        // Add x-axis
         select(this.node)
             .append("g")
             .attr("transform", "translate(0," + (height - padding) + ")")
-            .call(xAxis);
+            .call(axisBottom(xScale));
 
-        // Find y and y-axis
-        let yAxis = axisLeft(y);
+        // Add y-axis
         select(this.node)
             .append("g")
             .attr("transform", "translate(" + padding + ",0)")
-            .call(yAxis);
+            .call(axisLeft(yScale));
 
-        // Finally add data points
-        //select(this.node)
-        //    .selectAll("circle")
-        //    .attr("cx", d => x(d[0]))
-        //    .attr("cy", d => y(d[1]))
-        //    .attr("r", 2);
+        // Line object
+        let lineFunc = line().x(d => xScale(d[0])).y(d => yScale(d[1]));
+
+        // Add line to plot
+        select(this.node)
+            .append("g")
+            .append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-width", 1.5)
+            .attr("d", lineFunc);
     }
 
     render(){
@@ -68,14 +75,16 @@ class ScatterPlot extends Component{
             <svg
                 ref={ node => this.node = node }
                 width="500"
-                height="500"
+                height="300"
             />
         );
     }
 }
 
 ScatterPlot.propTypes = {
-    data: PropTypes.object
+    data: PropTypes.object,
+    width: PropTypes.string,
+    height: PropTypes.string
 };
 
 export default ScatterPlot;
--- src/js/Components/ScatterPlot/Utilities/utilities.js
+++ src/js/Components/ScatterPlot/Utilities/utilities.js
@@ -5,11 +5,9 @@ import { max, min }    from "d3-array";
 // Find xScale
 export function findXScale(data, width = 500, padding = 10){
 
-    let lastEntry = data.length - 1;
-    let minDate   = data[0][0];
-    let maxDate   = data[lastEntry][0];
-    let scale     = scaleTime().domain([minDate, maxDate]);
-
+    let xMin      = min(data, d => d[0]);
+    let xMax      = max(data, d => d[0]);
+    let scale     = scaleTime().domain([xMin, xMax]);
     scale.range([padding, width - padding]);
 
     return(scale);
@@ -21,7 +19,6 @@ export function findYScale(data, height = 460, padding = 10){
     let yMin  = min(data, d => d[1]);
     let yMax  = max(data, d => d[1]);
     let scale = scaleLinear().domain([yMin, yMax]);
-
     scale.range([height - padding, padding]);
 
     return(scale);
