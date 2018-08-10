ef4e856c26714c0a2f65ace56242a9466578c8c5
Pablo Lorenzo <lorenz3192@gmail.com>
2018-08-09 18:45:44 -0700

    Added Table/Success to handle table rendering

--- src/js/Components/Table/Table.js
+++ src/js/Components/Table/Table.js
@@ -1,44 +1,10 @@
-import React      from "react";
-import PropTypes  from "prop-types";
-import { Errors } from "./Errors";
+import React       from "react";
+import PropTypes   from "prop-types";
+import { Errors }  from "./Errors";
+import { Success } from "./Success";
 import "./table.scss";
 
-// Show table when data is retrieved
-const Success = (props) => {
-    let stockName = props.data[0][1]["2. Symbol"];
-    let stockData = Object.entries(props.data[1][1]).reverse();
-    return(
-        <table>
-            <caption>{ stockName }</caption>
-            <thead>
-                <tr>
-                    <th>Dates</th>
-                    <th>Open</th>
-                    <th>High</th>
-                    <th>Low</th>
-                    <th>Adjusted Close</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    stockData.map( (item, index) =>
-                        <tr key={ index }>
-                            <td>{ item[0] }</td>
-                            <td>{ item[1]["1. open"] }</td>
-                            <td>{ item[1]["2. high"] }</td>
-                            <td>{ item[1]["3. low"] }</td>
-                            <td>{ item[1]["5. adjusted close"] }</td>
-                        </tr>
-                    )
-                }
-            </tbody>
-        </table>
-    );
-};
-
-// Main component
 const Table = (props) => {
-
     // Store data
     let stockData = props.data;
 
@@ -46,7 +12,7 @@ const Table = (props) => {
     if(stockData[0] !== undefined){
         if(stockData[0][0] === "Error Message"){
             return(
-                <Errors error={stockData}/>
+                <Errors error={ stockData }/>
             );
         }
         else{
