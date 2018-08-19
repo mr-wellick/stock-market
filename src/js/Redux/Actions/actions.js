937f56cdcdfa8a774016ec4feda2248439e004a1
Pablo Lorenzo <lorenz3192@gmail.com>
2018-08-19 03:19:45 -0700

    work on getting basic functionality with redux first

--- src/js/Redux/Actions/actions.js
+++ src/js/Redux/Actions/actions.js
@@ -12,37 +12,39 @@ export let initialState = {
     errorMessage: ""
 };
 
-// Shape for stock data only
-export let stocks = {
-    stockDataOnly: "function=TIME_SERIES_",
-    dataType:
-    [
-        { monthly: "MONTHLY_ADJUSTED" },
-        { daily: "DAILY_ADJUSTED" },
-    ]
-};
-
-// Shape for crypto
-export let crypto = {
-    cryptocurrency: "function=DIGITAL_CURRENCY_",
-    dataType:
-    [
-        { monthly: "MONTHLY" },
-        { daily: "DAILY" }
-    ]
-};
-
-// What actions do we need?
+// Main actions and error handling.
 export let GET_USER_INPUT      = "GET_USER_INPUT";
 export let REQUEST_STOCK_DATA  = "REQUEST_STOCK_DATA";
 export let RECEIVED_STOCK_DATA = "RECEIVED_STOCK_DATA";
 
+
+// Shape for stock data only
+//export let stocks = {
+//    stockDataOnly: "function=TIME_SERIES_",
+//    dataType:
+//    [
+//        { monthly: "MONTHLY_ADJUSTED" },
+//        { daily: "DAILY_ADJUSTED" },
+//    ]
+//};
+
+// Shape for crypto
+//export let crypto = {
+//    cryptocurrency: "function=DIGITAL_CURRENCY_",
+//    dataType:
+//    [
+//        { monthly: "MONTHLY" },
+//        { daily: "DAILY" }
+//    ]
+//};
+
+
 // Choose stock only.
-export let CHOOSE_STOCKS_ONLY      = "CHOOSE_STOCKS_ONLY";
-export let CHOOSE_MONTHLY_ADJUSTED = "CHOOSE_MONTHLY_ADJUSTED";
-export let CHOOSE_DAILY_ADJUSTED   = "CHOOSE_DAILY_ADJUSTED";
+//export let CHOOSE_STOCKS_ONLY      = "CHOOSE_STOCKS_ONLY";
+//export let CHOOSE_MONTHLY_ADJUSTED = "CHOOSE_MONTHLY_ADJUSTED";
+//export let CHOOSE_DAILY_ADJUSTED   = "CHOOSE_DAILY_ADJUSTED";
 
 // Choose cryptocurrency only.
-export let CHOOSE_CRYPTOCURRENCY_ONLY = "CHOOSE_CRYPTOCURRENCY_ONLY";
-export let CHOOSE_MONTHLY             = "CHOOSE_MONTHLY";
-export let CHOOSE_DAILY               = "CHOOSE_DAILY";
+//export let CHOOSE_CRYPTOCURRENCY_ONLY = "CHOOSE_CRYPTOCURRENCY_ONLY";
+//export let CHOOSE_MONTHLY             = "CHOOSE_MONTHLY";
+//export let CHOOSE_DAILY               = "CHOOSE_DAILY";
