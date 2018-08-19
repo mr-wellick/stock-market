0059f3412d12b0c158153ea21be567af65aafc62
Pablo Lorenzo <lorenz3192@gmail.com>
2018-08-19 02:27:56 -0700

    working on application actions

---
+++ src/js/Redux/Actions/actions.js
@@ -0,0 +1,12 @@
+// What actions does out application need?
+export let GET_USER_INPUT          = "GET_USER_INPUT";
+export let REQUEST_STOCK_DATA      = "REQUEST_STOCK_DATA";
+export let RECEIVED_STOCK_DATA     = "RECEIVED_STOCK_DATA";
+
+// Now, need to handle errors in application.
+
+
+// This comes last.
+export let CHOOSE_MONTHLY_ADJUSTED = "CHOOSE_MONTHLY_ADJUSTED";
+export let CHOOSE_DAILY_ADJUSTED   = "CHOOSE_DAILY_ADJUSTED";
+export let CHOOSE_BATCH_QUOTES     = "CHOOSE_BATCH_QUOTES";
