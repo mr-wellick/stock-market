3feb716f156d6082bbecb04e03428018793edb07
Pablo Lorenzo <lorenz3192@gmail.com>
2018-08-21 04:09:06 -0700

    updated form input field default helper text

--- src/js/Components/Form/Form.js
+++ src/js/Components/Form/Form.js
@@ -6,7 +6,7 @@ const Form = (props) => {
     return(
         <div className="input-container">
             <form onSubmit={ props.onSubmit }>
-                <input type="text" id="section__form-input" placeholder="Enter a stock ticker"/>
+                <input type="text" id="section__form-input" placeholder="Enter a valid ticker"/>
                 <input type="submit" value="View"/>
             </form>
         </div>
