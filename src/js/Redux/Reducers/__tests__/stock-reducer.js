b8c186595896c7d7b16b226d324b3f893754f848
Pablo Lorenzo <lorenz3192@gmail.com>
2018-08-29 10:31:36 -0700

    removing user-input/user-selection reducers in favor of stock-reducer

---
+++ src/js/Redux/Reducers/__tests__/stock-reducer.js
@@ -0,0 +1,32 @@
+import stockReducer from "../stock-reducer";
+
+test("test initial asset name is TSLA", () => {
+    let name                = "TSLA"
+    let nextState           = stockReducer(initialState, incorrectAction);
+    let { userInteraction } = nextState;
+    let { assetName }       = userInteraction;
+    expect(assetName).toBe(name);
+});
+
+test("test initial asset type to be monthly adjusted", () => {
+    let type                = "function=TIME_SERIES_MONTHLY_ADJUSTED&";
+    let nextState           = stockReducer(initialState, incorrectAction);
+    let { userInteraction } = nextState;
+    let { assetType }       = userInteraction;
+    expect(assetType).toBe(type);
+});
+
+//test("test that state is updated correctly", () => {
+//});
+
+let initialState = {
+    userInteraction: {
+        assetName: "TSLA",
+        assetType: "function=TIME_SERIES_MONTHLY_ADJUSTED&"
+    }
+};
+
+let incorrectAction = {
+    type: "INCORRECT_ACTION",
+    assetName: "APPL"
+};
---
+++ src/js/Redux/Reducers/stock-reducer.js
@@ -0,0 +1,27 @@
+import { USER_INPUT }     from "../Constants";
+//import { USER_SELECTION } from "../Constants";
+
+function stockReducer(
+    state = {
+        userInteraction: {
+            assetName: "TSLA",
+            assetType: "function=TIME_SERIES_MONTHLY_ADJUSTED&"
+        }
+    },
+    action
+)
+{
+    switch(action.type)
+    {
+        case USER_INPUT:
+            return Object.assign({}, state, {
+                userInteraction: {
+                    assetName: action.assetName
+                }
+            });
+        default:
+            return state;
+    }
+}
+
+export default stockReducer;
