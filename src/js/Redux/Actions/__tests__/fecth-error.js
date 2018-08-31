9b407768c5695c57436d1869e3635fa9d8881738
Pablo Lorenzo <lorenz3192@gmail.com>
2018-08-31 06:51:36 -0700

    completed fetch-error.js test

--- src/js/Redux/Actions/__tests__/fecth-error.js
+++ src/js/Redux/Actions/__tests__/fecth-error.js
@@ -3,10 +3,15 @@ import { fetchError }  from "../Fetch-Data";
 import { errorData }   from "../Fetch-Data";
 
 // Need to add more tests
-test("make sure data is process", () => {
+test("make sure fetch error returns correct object properties", () => {
     let dataReturnedByAPI = errorData;
     let dataProcessed     = Object.entries(dataReturnedByAPI);
     let errorAction       = fetchError(dataProcessed); 
-    let { type }          = errorAction;
-    expect(type).toBe(FETCH_ERROR);
+
+    // Check correct properties are generated
+    expect(errorAction).toHaveProperty("type", FETCH_ERROR);
+    expect(errorAction).toHaveProperty("assetData");
+    expect(errorAction).toHaveProperty("error", true);
+    expect(errorAction).toHaveProperty("errorMessage");
+    expect(errorAction).toHaveProperty("processedData");
 });
