fc130ddfbab816f0cbdaf9d78ee68f55d8c8fea5
Pablo Lorenzo <lorenz3192@gmail.com>
2018-08-31 07:34:38 -0700

    action creators complete

--- src/js/Redux/Actions/Fetch-Data/fetch-data.js
+++ src/js/Redux/Actions/Fetch-Data/fetch-data.js
@@ -1,16 +1,22 @@
-//import { store }   from "../../Reducers";
-import userInput  from "../user-input";
-import fetchError from "./fetch-error";
-import  fetchSuccess from "./fetch-success";
-//let url    = "https://www.alphavantage.co/query?";
-//let symbol = `symbol${assetName}&`
-//let apiKey = "apikey=AAG3PU4MLMB9JHS3";
+import { store }    from "../../Reducers";
+import userInput    from "../user-input";
+import fetchError   from "./fetch-error";
+import fetchSuccess from "./fetch-success";
+
+// API info for data request
+let url    = "https://www.alphavantage.co/query?";
+let apiKey = "apikey=AAG3PU4MLMB9JHS3";
 
 // Make api request
 function fetchData(assetName)
 {
     // Get asset type request: monthly or daily.
-    //let assetType = store.getState();
+    // Symbol used for either stocks or cryptocurrency
+    let assetType = store.getState();
+    let symbol    = `symbol${assetName}&`;
+
+    // Full url to use in request
+    let fullURL = url + assetType + symbol + apiKey;
 
     return function(dispatch){
 
@@ -18,7 +24,7 @@ function fetchData(assetName)
         dispatch(userInput(assetName));
 
         //2. Next, request data
-        return fetch().then(res => res.json())
+        return fetch(fullURL).then(res => res.json())
                       .then(data => {
                         // 3. Dispatch error or success
                         if(data["Error Message"])
--- src/js/Redux/Actions/Fetch-Data/index.js
+++ src/js/Redux/Actions/Fetch-Data/index.js
@@ -1,4 +1,5 @@
 export { default as fetchError }   from "./fetch-error";
 export { default as fetchSuccess } from "./fetch-success";
+export { default as fetchData }    from "./fetch-data";
 export { default as errorData }    from "./Testing-Data/error.json";
 export { default as msftData }     from "./Testing-Data/msft.json";
