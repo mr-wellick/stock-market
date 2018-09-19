aac26ba4381c6086b5265ec3cfb27be0e5105d2a
Pablo Lorenzo <lorenz3192@gmail.com>
2018-09-18 20:18:11 -0700

    updated props

--- src/js/Views/Home/Home.js
+++ src/js/Views/Home/Home.js
@@ -39,7 +39,7 @@ class Home extends Component{
             <Fragment>
                 <section className="section-forms">
                     <Select/>
-                    <Input onSubmit={ this.onSubmit } placeholder="Enter ticker"/>
+                    <Input onSubmit={ this.onSubmit } placeholder="Enter ticker(s)"/>
                 </section>
                 <section className="section-data">
                     {
