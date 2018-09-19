3581e10baaf14ced7a6f7e4d47edb4a0d533d107
Pablo Lorenzo <lorenz3192@gmail.com>
2018-09-18 17:05:17 -0700

    updated fetchComplete action creator & tested

--- src/js/Redux/Actions/__tests__/fetch-complete.js
+++ src/js/Redux/Actions/__tests__/fetch-complete.js
@@ -1,5 +1,18 @@
-import fetchComplete from "../fetch-complete.js";
+import fetchComplete   from "../fetch-complete.js";
+import { FETCH_COMPLETE } from "../../Constants";
+import fetchSuccess    from "../fetch-success.js";
+import fetchError      from "../fetch-error.js";
+import { msftData }    from "../Testing-Data";
+import { errorData }   from "../Testing-Data";
+
 
 test("need to refactor fetch complete", () => {
-    console.log(fetchComplete("this is a test"));
+    let successApiCall = fetchSuccess(msftData);
+    let errorApiCall   = fetchError(errorData);
+    let dataArray      = [ successApiCall, errorApiCall ]; // Need an array since that's how our final data is stored
+    let dataToDispatch = fetchComplete(dataArray);
+
+    expect(dataToDispatch).toHaveProperty("type", FETCH_COMPLETE);
+    expect(dataToDispatch).toHaveProperty("error");
+    expect(dataToDispatch).toHaveProperty("tooManyCalls");
 });
--- src/js/Redux/Actions/fetch-complete.js
+++ src/js/Redux/Actions/fetch-complete.js
@@ -1,10 +1,20 @@
-import { FETCH_COMPLETE } from "../Constants";
+import { FETCH_COMPLETE }       from "../Constants";
+import { FETCH_SUCCESS }        from "../Constants";
+import { FETCH_ERROR }          from "../Constants";
+import { FETCH_TOO_MANY_CALLS } from "../Constants";
 
 function fetchComplete(assetsData)
 {
+    // Filter data
+    let success      = assetsData.filter( dataSet => dataSet["type"] === FETCH_SUCCESS );
+    let error        = assetsData.filter( dataSet => dataSet["type"] === FETCH_ERROR );
+    let tooManyCalls = assetsData.filter( dataSet => dataSet["type"] === FETCH_TOO_MANY_CALLS );
+
     return {
         type: FETCH_COMPLETE,
-        assetsData
+        success,
+        error,
+        tooManyCalls
     };
 }
 
