13931c0dd6d6906b8085e2358ce6791d00f75b5c
Pablo Lorenzo <lorenz3192@gmail.com>
2018-09-22 06:11:15 -0700

    refactored into class components

--- src/js/Components/DroppedStock/DroppedStock.js
+++ src/js/Components/DroppedStock/DroppedStock.js
@@ -1,19 +1,19 @@
-import React     from "react";
-import PropTypes from "prop-types";
+import React         from "react";
+import { Component } from "react";
 import "./droppedStock.scss";
 
-let DroppedStock = (props) => {
-    return(
-        <div className="dropped-stock">
-            <div className={ true ? "dropped-stock__message--hide" : "" }>
-                <strong>Error!</strong> Entered incorrect stock.
+class DroppedStock extends Component{
+    render(){
+        return(
+            <div className="dropped-stock">
+                <div>
+                    <strong>Error!</strong> Entered incorrect stock.
             </div>
             <a className="dropped-stock__message--toggler">X</a>
         </div>
-    );
-};
+        );
+    }
+}
 
-DroppedStock.propTypes = {
-};
 
 export default DroppedStock;
--- src/js/Components/ExceededCallLimit/ExceededCallLimit.js
+++ src/js/Components/ExceededCallLimit/ExceededCallLimit.js
@@ -3,20 +3,14 @@ import { Component } from "react";
 import "./exceededCallLimit.scss";
 
 class ExceededCallLimit extends Component{
-
-    onClick = () => {
-        let node = document.querySelector(".too-many-calls");
-        console.log(node);
-    }
-
     render(){
         return(
             <div className="too-many-calls">
-                <div className={ true ? "too-many-calls__message--hide" : "" }>
+                <div>
                     <strong>Warning!</strong> Exceeded call Limit. Please wait a few seconds and try again.
+                </div>
+                <a className="too-many-calls__message--toggler">X</a>
             </div>
-            <a className="too-many-calls__message--toggler" onClick={ this.onClick }>X</a>
-        </div>
         );
     }
 }
