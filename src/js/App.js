a0c0c671f7fc60198ff2af6d3a40897d9318338c
Pablo Lorenzo <lorenz3192@gmail.com>
2018-09-27 10:34:27 -0700

    React-Router: added HashRouter to deal with GitHub pages issue

--- src/js/App.js
+++ src/js/App.js
@@ -1,13 +1,13 @@
 import "./app.scss";
-import React, { Component, Fragment }           from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Home }                                 from "./Views";
-import { Footer }                               from "./Components";
+import React, { Component, Fragment }      from "react";
+import { HashRouter, Route, Link, Switch } from "react-router-dom";
+import { Home }                            from "./Views";
+import { Footer }                          from "./Components";
 
 class App extends Component{
     render(){
         return(
-            <Router>
+            <HashRouter>
                 <Fragment>
                     <header className="main-header">
                         <h1 className="main-header__logo--display">&sigma; Stocks</h1>
@@ -17,22 +17,24 @@ class App extends Component{
                                     <Link to="/">Home</Link>
                                 </li>
                                 <li className="main-nav__item">
-                                    <Link to="/">Basics</Link>
+                                    <Link to="/Basics">Basics</Link>
                                 </li>
                                 <li className="main-nav__item">
-                                    <Link to="/">Portfolio</Link>
+                                    <Link to="/Portfolio">Portfolio</Link>
                                 </li>
                             </ul>
                         </nav>
                     </header>
-                    <Route exact path="/" component={ Home }></Route>
-                    {/*
-                        <Route exact path="/Basics" component={ Basics }></Route>
-                        <Route exact path="/Portfolio" component={ Portfolio }></Route>
-                    */}
+                    <Switch>
+                        <Route exact path="/" component={ Home }></Route>
+                        {/*
+                            <Route exact path="/Basics" component={ Basics }></Route>
+                            <Route exact path="/Portfolio" component={ Portfolio }></Route>
+                        */}
+                    </Switch>
                     <Route path="/" component={ Footer }></Route>
                 </Fragment>
-            </Router>
+            </HashRouter>
         );
     }
 }
