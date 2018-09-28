7ccb64a9d5722ab8568d7fb1a4491f5b596dd582
Pablo Lorenzo <lorenz3192@gmail.com>
2018-09-28 02:40:27 -0700

    added temporary fix to persist fetched data

--- src/js/Redux/Actions/fetch-complete.js
+++ src/js/Redux/Actions/fetch-complete.js
@@ -2,6 +2,7 @@ import { FETCH_COMPLETE }       from "../Constants";
 import { FETCH_SUCCESS }        from "../Constants";
 import { FETCH_ERROR }          from "../Constants";
 import { FETCH_TOO_MANY_CALLS } from "../Constants";
+import { store }                from "../Reducers";
 
 function fetchComplete(assetsData)
 {
@@ -9,6 +10,11 @@ function fetchComplete(assetsData)
     let successData      = assetsData.filter( dataSet => dataSet["type"] === FETCH_SUCCESS );
     let errorData        = assetsData.filter( dataSet => dataSet["type"] === FETCH_ERROR );
     let tooManyCallsData = assetsData.filter( dataSet => dataSet["type"] === FETCH_TOO_MANY_CALLS );
+
+    // Merge successData with current state.
+    // Note: temporary fix
+    let dataFromStore = store.getState().receivedData.successData;
+    successData       = [...dataFromStore, ...successData];
 
     return {
         type: FETCH_COMPLETE,
