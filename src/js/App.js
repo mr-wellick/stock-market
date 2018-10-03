dd2bd562bb3f16fffbaf7a41308b37326b8847cf
Pablo Lorenzo <lorenz3192@gmail.com>
2018-10-03 15:40:20 -0700

    added nav-bar scss for large displays

--- src/js/App.js
+++ src/js/App.js
@@ -13,22 +13,24 @@ class App extends Component{
         return(
             <HashRouter>
                 <Fragment>
-                    <header className="main-header">
-                        <h1 className="main-header__logo">Stocks</h1>
-                        <nav className="main-nav">
-                            <ul className="main-nav__items"> 
-                                <li className="main-nav__item">
-                                    <Link to="/">Home</Link>
-                                </li>
-                                <li>
-                                    <p>Basics</p>
-                                </li>
-                                <li>
-                                    <p>Portfolio</p>
-                                </li>
-                            </ul>
-                        </nav>
-                    </header>
+                    <div className="main-header-container">
+                        <header className="main-header">
+                            <h1 className="main-header__logo">Stocks</h1>
+                            <nav className="main-nav">
+                                <ul className="main-nav__items"> 
+                                    <li className="main-nav__item">
+                                        <Link to="/">Home</Link>
+                                    </li>
+                                    <li>
+                                        <p>Basics</p>
+                                    </li>
+                                    <li>
+                                        <p>Portfolio</p>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </header>
+                    </div>
                     <Switch>
                         <Route exact path="/" component={ Home }></Route>
                     </Switch>
