a0ad4d3e2ad5602a4b7f509e7d05f7b442620839
Pablo Lorenzo <lorenz3192@gmail.com>
2018-10-03 20:29:02 -0700

    begun fetchData action creator

--- src/js/Redux/Actions/fetchData.js
+++ src/js/Redux/Actions/fetchData.js
@@ -1,8 +1,25 @@
-import { FETCH_DATA_REQUEST } from "../Constants/";
-
 function fetchData(stockNames)
 {
-    console.log(stockNames);
+    // get asset type to request: monthly or daily
+    let assetType;
+
+    return function(dispatch){
+        // begin request
+        return(
+            // iterate through each stock and request
+            Promise.all(
+                stockNames.map(stock =>
+                    fetch(`https://www.alphavantage.co/query?${assetType}symbol=${stock}&apikey=${process.env.API_KEY}`)
+                    .then(res => res.json())
+                )
+            )
+            // process data accordingly
+            .then(allStockData => {
+                console.log(allStockData);
+            })
+            // end request
+        );
+    };
 }
 
 export default fetchData;
