7b1959bdc674775f1ba64bc0a4d64ea7179dc8bf
Pablo Lorenzo <lorenz3192@gmail.com>
2018-10-04 11:45:26 -0700

    removed unecessary user of Object.assign()

--- src/js/Redux/Reducers/fetchDataReducer.js
+++ src/js/Redux/Reducers/fetchDataReducer.js
@@ -13,17 +13,17 @@ function fetchDataReducer(state = initialState, action)
     switch(action.type)
     {
         case FETCH_DATA_COMPLETE:
-            return Object.assign({}, state, {
+            return {
                 ...state,
                 successData: [...state.successData, ...action.successData],
                 errorData: action.errorData,
                 manyCallsData: action.manyCallsData
-            });
+            };
         case FETCH_DATA_REQUEST:
-            return Object.assign({}, state, {
+            return {
                 ...state,
                 isFetching: action.isFetching
-            });
+            };
         default:
             return state;
     }
