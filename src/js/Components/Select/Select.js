2988cb4cc2b43884425060713e61595b24228ae5
Pablo Lorenzo <lorenz3192@gmail.com>
2018-10-03 18:41:46 -0700

    format code

--- src/js/Components/Select/Select.js
+++ src/js/Components/Select/Select.js
@@ -1,35 +1,31 @@
 import React         from "react";
 import { Component } from "react";
-import { Fragment }  from "react";
 import "./select.scss";
 
 class Select extends Component{
     render(){
         return(
-            <Fragment>
-                <form className="selection-form">
-                    <div className="selection-form__monthly">
-                        <input
+            <form className="selection-form">
+                <div className="selection-form__monthly">
+                    <input
+                        type="radio"
+                        id="monthly"
+                        name="stock-type"
+                        value="function=TIME_SERIES_MONTHLY_ADJUSTED&"
+                        defaultChecked
+                    />
+                    <label htmlFor="monthly">Monthly</label>
+                </div>
+                <div className="selection-form__daily">
+                    <input
                             type="radio"
-                            id="monthly"
+                            id="daily"
                             name="stock-type"
-                            value="function=TIME_SERIES_MONTHLY_ADJUSTED&"
-                            defaultChecked
+                            value="function=TIME_SERIES_DAILY_ADJUSTED&"
                         />
-                        <label htmlFor="monthly">Monthly</label>
-                    </div>
-                    <div className="selection-form__daily">
-                        <input
-                                type="radio"
-                                id="daily"
-                                name="stock-type"
-                                value="function=TIME_SERIES_DAILY_ADJUSTED&"
-                            />
-                        <label htmlFor="daily">Daily</label>
-                    </div>
-                </form>
-
-            </Fragment>
+                    <label htmlFor="daily">Daily</label>
+                </div>
+            </form>
         );
     }
 }
