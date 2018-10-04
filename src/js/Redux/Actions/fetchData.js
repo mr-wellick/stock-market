50b9d2eb1e05d77321d494518a465b85b783e661
Pablo Lorenzo <lorenz3192@gmail.com>
2018-10-03 20:16:59 -0700

    added redux actions

---
+++ src/js/Redux/Actions/fetchData.js
@@ -0,0 +1,8 @@
+import { FETCH_DATA_REQUEST } from "../Constants/";
+
+function fetchData(stockNames)
+{
+    console.log(stockNames);
+}
+
+export default fetchData;
---
+++ src/js/Redux/Constants/index.js
@@ -0,0 +1,11 @@
+// api request actions
+export const FETCH_DATA_REQUEST        = "FETCH_DATA_REQUEST";
+export const FETCH_DATA_SUCCESS        = "FETCH_DATA_SUCCESS";
+export const FETCH_DATA_ERROR          = "FETCH_DATA_ERROR";
+export const FETCH_DATA_TOO_MANY_CALLS = "FETCH_DATA_TOO_MANY_CALLS";
+
+// user interaction actions
+export const USER_INPUT_STOCKS             = "USER_INPUT_STOCKS";
+export const USER_SELECT_DATA_TYPE         = "USER_SELECT_DATA_TYPE";
+export const USER_ENTERED_DUPLICATE_STOCKS = "USER_ENTERED_DUPLICATE_STOCKS";
+export const USER_SELECT_STOCK_TO_DISPLAY  = "USER_SELECT_STOCK_TO_DISPLAY";
