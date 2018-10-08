c49a969e5cb13c837ab27e3f2404dbcd5c7eb72c
Pablo Lorenzo <lorenz3192@gmail.com>
2018-10-07 21:48:42 -0700

    code formatting

--- src/js/Views/Home/Home.js
+++ src/js/Views/Home/Home.js
@@ -4,7 +4,7 @@ import { Fragment }    from "react";
 import { Input }       from "../../Components/";
 import { Dialog }      from "../../Components/";
 import { Select }      from "../../Components/";
-import { Loader }    from "../../Components/";
+import { Loader }      from "../../Components/";
 import { Table }       from "../../Components/";
 import { Toggler }     from "../../Components/";
 import { ActiveStock } from "../../Components/";
