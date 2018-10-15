12c8ba8211aac9ad382f862e7f4d7797e58f2779
Pablo Lorenzo <lorenz3192@gmail.com>
2018-10-15 01:01:31 -0700

    code formatting

--- src/js/Components/ActiveStock/ActiveStock.js
+++ src/js/Components/ActiveStock/ActiveStock.js
@@ -1,6 +1,6 @@
-import React           from "react";
-import { Component }   from "react";
-import PropTypes       from "prop-types";
+import React         from "react";
+import { Component } from "react";
+import PropTypes     from "prop-types";
 import "./activeStock.scss";
 
 class ActiveStock extends Component{
