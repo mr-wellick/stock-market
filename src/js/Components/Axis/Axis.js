39635304bdba590e884ece393903d0530f24254b
Pablo Lorenzo <lorenz3192@gmail.com>
2018-10-17 19:54:49 -0700

    refactored for readability

--- src/js/Components/Axis/Axis.js
+++ src/js/Components/Axis/Axis.js
@@ -9,43 +9,41 @@ import { transition } from "d3-transition";
 import "./axis.scss";
 
 class Axis extends Component{
-    findScale(){
+    findXAxis(){
         let { scale }         = this.props;
         let { width, height } = this.props;
         let { padding }       = this.props;
-        let { axis }          = this.props;
         let axisLocation;
 
-        if(axis === "x-axis")
-        {
-            // Calculate x-axis
-            axisLocation = "translate(0," + (height - padding) + ")";
-            scale.range([padding, width - padding]).nice();
+        // Calculate x-axis
+        axisLocation = "translate(0," + (height - padding) + ")";
+        scale.range([padding, width - padding]).nice();
 
-            // Format ticks based on daily or monthly data and append x-axis
-            select(".x-axis")
-                .transition()
-                .duration(200)
-                .attr("transform", axisLocation)
-                .call(
-                    axisBottom(scale)
-                    /*.tickFormat(timeFormat("%y"))*/ // Slight issues with this line of code
-                );
-        }
-        else if(axis === "y-axis")
-        {
-            // Calculate y-axis
-            axisLocation = "translate(" + padding + ",0)";
-            scale.range([height - padding, padding]).nice();
+        // Append x-axis
+        select(".x-axis")
+            .transition()
+            .duration(200)
+            .attr("transform", axisLocation)
+            .call(axisBottom(scale)/*.tickFormat(timeFormat("%y"))*/);
+    }
 
-            // Append y-axis
-            select(".y-axis")
-                .transition()
-                .delay(200)
-                .duration(300)
-                .attr("transform", axisLocation)
-                .call(axisLeft(scale));
-        }
+    findYAxis(){
+        let { scale }   = this.props;
+        let { height }  = this.props;
+        let { padding } = this.props;
+        let axisLocation;
+
+        // Calculate y-axis
+        axisLocation = "translate(" + padding + ",0)";
+        scale.range([height - padding, padding]).nice();
+
+        // Append y-axis
+        select(".y-axis")
+            .transition()
+            .delay(200)
+            .duration(300)
+            .attr("transform", axisLocation)
+            .call(axisLeft(scale));
     }
 
     render(){
@@ -55,11 +53,21 @@ class Axis extends Component{
     }
 
     componentDidMount(){
-        this.findScale();
+        let { axis } = this.props;
+
+        if(axis === "x-axis")
+            this.findXAxis();
+        else if(axis === "y-axis")
+            this.findYAxis();
     }
 
     componentDidUpdate(){
-        this.findScale();
+        let { axis } = this.props;
+
+        if(axis === "x-axis")
+            this.findXAxis();
+        else if(axis === "y-axis")
+            this.findYAxis();
     }
 }
 
