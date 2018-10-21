84f689408ec8382acc9e5d49a63396964c7bb128
Pablo Lorenzo <lorenz3192@gmail.com>
2018-10-20 19:42:49 -0700

    added uglify js

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,12 +1,16 @@
 let extractCSS  = require("mini-css-extract-plugin");
 let minimizeCSS = require("optimize-css-assets-webpack-plugin");
+let uglifyJS    = require("uglifyjs-webpack-plugin");
 
 module.exports = () => ({
     optimization:
     {
         minimizer:
         [
-            new minimizeCSS({})
+            new minimizeCSS({}),
+            new uglifyJS({
+                parallel: true
+            })
         ]
     },
     module:
