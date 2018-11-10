12bf8e2fadcd67d81e6db26d7ff64aab76e8e9b9
Pablo Lorenzo <lorenz3192@gmail.com>
2018-11-10 15:32:50 -0800

    added css

--- src/js/Components/StockDescription/StockDescription.js
+++ src/js/Components/StockDescription/StockDescription.js
@@ -1,6 +1,7 @@
 import React         from "react";
 import PropTypes     from "prop-types";
 import { Component } from "react";
+import "./stockDescription.scss";
 
 class StockDescription extends Component{
     static propTypes = {
@@ -13,12 +14,14 @@ class StockDescription extends Component{
             return null;
 
         return(
-            <div>
-                <h2>
-                    { `${this.props.successData["data"]["stockName"]} founded in ${this.props.robinhoodData["year_founded"]}` }
+            <div className="description-container">
+                <h2 className="stock-founded">
+                    {`${this.props.successData["data"]["stockName"]} founded in ${this.props.robinhoodData["year_founded"]}`}
                 </h2>
-                <h4>{ `CEO: ${this.props.robinhoodData["ceo"]}` }</h4>
-                <p>{ this.props.robinhoodData["description"] }</p>
+                <div className="main-info-container">
+                    <h4 className="stock-ceo">{ `CEO: ${this.props.robinhoodData["ceo"]}` }</h4>
+                    <p className="stock-description">{ this.props.robinhoodData["description"] }</p>
+                </div>
             </div>
         );
     }
