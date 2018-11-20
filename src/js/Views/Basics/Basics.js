37f0f709ae84eec33900fec2c715cdbd3e254acd
Pablo Lorenzo <lorenz3192@gmail.com>
2018-11-19 21:26:14 -0800

    started sub routes

--- src/js/Views/Basics/Basics.js
+++ src/js/Views/Basics/Basics.js
@@ -1,16 +1,39 @@
-import React         from "react";
-import { Component } from "react";
-import Intro         from "./Intro.js";
-import Resources     from "./Resources.js";
+import React          from "react";
+import PropTypes      from "prop-types";
+import { Component }  from "react";
+import { Route }      from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import { Switch }     from "react-router-dom";
+import { Link }       from "react-router-dom";
+import Intro          from "./Intro.js";
+import Resources      from "./Resources.js";
 import "./basics.scss";
 
 class Basics extends Component{
+    static propTypes = {
+        match: PropTypes.object
+    }
+
     render(){
+        let { match } = this.props;
+
         return(
-            <section>
-                <Intro/>
-                <Resources/>
-            </section>
+                <>
+                <section style={{ marginTop: "75px" }}>
+                    <header>
+                        <ul>
+                            <li>
+                                <Link to={ `${match.url}/Intro` }>Intro</Link>
+                            </li>
+                            <li>
+                                <Link to={ `${match.url}/Resources` }>Resources</Link>
+                            </li>
+                        </ul>
+                    </header>
+                </section>
+                <Route path={ `${match.path}` } component={ Intro }/>
+                <Route path={ `${match.path}` } component={ Resources }/>
+                </>
         );
     }
 }
