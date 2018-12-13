e7d8e1c557b44925acd94fe9a2b2e53b3d382523
Pablo Lorenzo <lorenz3192@gmail.com>
2018-12-12 23:59:14 -0800

    using new react api context

--- src/js/Views/Home/Home.js
+++ src/js/Views/Home/Home.js
@@ -1,69 +1,70 @@
 import React                from "react";
 import { Component }        from "react";
-import { Loader }           from "../../Components/";
-import { StockMetrics }     from "../../Components/";
-import { Toggler }          from "../../Components/";
-import { StockSelector }    from "../../Components/";
+//import { Loader }           from "../../Components/";
+//import { StockMetrics }     from "../../Components/";
+//import { Toggler }          from "../../Components/";
+//import { StockSelector }    from "../../Components/";
 //import { StockDescription } from "../../Components/";
 //import { StockNews }        from "../../Components/";
-import { DuplicateEntry }   from "../../Components/";
-import { Errors }           from "../../Components/";
-import { ChartSelector }    from "../../Components/";
-import { ActiveChart }      from "../../Components/";
+//import { DuplicateEntry }   from "../../Components/";
+//import { Errors }           from "../../Components/";
+//import { ChartSelector }    from "../../Components/";
+//import { ActiveChart }      from "../../Components/";
 import "./home.scss";
 
 class Home extends Component{
+    state = {
+        stockMarketData: [],
+        activeIndex: 0,
+        errors: "",
+        success: "",
+        duplicateEntry: "",
+        isFetchingData: false,
+    }
+
+    resetApplicationMessages(messageType){
+        if(messageType === "ERRORS")
+            this.setState({ errors: "" });
+
+        if(messageType === "SUCCESS")
+            this.setState({ success: "" });
+
+        if(messageType === "duplicateEntry");
+            this.setState({ duplicateEntry: "" });
+    }
+
+    async fetchStockMarketData(stockName){
+        // indicate we are fetching data
+        this.setState({ isFetchingData: true });
+
+        const res = await fetch(
+            `https://api.iextrading.com/1.0/stock/${
+                stockName
+            }/batch?types=quote,news,company,stats,financials,relevant,chart&range=5y`
+        );
+
+        try{
+            // successful request
+            const singleStockData = await res.json();
+            this.setState({
+                stockMarketData: this.state.stockMarketData.push(singleStockData),
+                success: `Successfully retrieved: ${stockName}`,
+                isFetchingData: false
+            });
+        }
+        catch(err){
+            // unsucessful request
+            this.setState({
+                errors: `Failed to retrieve: ${stockName}`,
+                isFetchingData: false
+            });
+        }
+    }
+
     render(){
         return(
-            <>
-                <Errors/>
-                <DuplicateEntry/>
-                <Toggler>
-                    { ({ stockData, onChange, activeIndex, width, height, padding, onChangeChart, selectedChart, deleteStock }) => (
-                        <>
-                            <StockMetrics
-                                stockData={ stockData[activeIndex] }
-                            />
-                            <div className="chart-data__container">
-                                <div className="chart">
-                                    <Loader/>
-                                    <div className="chart-stock-selection__container">
-                                        <StockSelector
-                                            stockData={ stockData }
-                                            onChange={ onChange }
-                                            deleteStock={ deleteStock }
-                                            activeIndex={ activeIndex }
-                                        />
-                                        <ChartSelector
-                                            onChangeChart={ onChangeChart }
-                                        />
-                                    </div>
-                                    <ActiveChart
-                                        width={ width }
-                                        height={ height }
-                                        padding={ padding }
-                                        activeStockData={ stockData[activeIndex] }
-                                        selectedChart={ selectedChart }
-                                        stockData={ stockData }
-                                    />
-                                </div>
-                                {/*
-                                    <div>
-                                        <StockDescription
-                                            stockData={ stockData[activeIndex] }
-                                        />
-                                    </div>
-                                    <div>
-                                        <StockNews
-                                            stockData={ stockData[activeIndex] }
-                                        />
-                                    </div>
-                                */}
-                            </div>
-                        </>
-                    )}
-                </Toggler>
-            </>
+            <section>
+            </section>
         );
     }
 }
