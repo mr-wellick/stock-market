1415e61bf4f54bad84bf44ba40c275f206b2ed93
Pablo Lorenzo <lorenz3192@gmail.com>
2019-01-17 19:16:02 -0800

    remove redux

--- src/js/Redux/index.js
+++
@@ -1,6 +0,0 @@
-export { fetchStockData }                 from "./Actions/";
-export { duplicateStockEntry }            from "./Actions/";
-export { resetDuplicateStockEntry }       from "./Actions/";
-export { resetFailedFetchRequestMessage } from "./Actions/";
-export { deleteSingleStock }              from "./Actions/";
-export { store }                          from "./Reducers/";
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,9 @@
 import "../css/style.scss";
 import React        from "react";
 import { render }   from "react-dom";
-import { Provider } from "react-redux";
-import { store }    from "./Redux";
 import App          from "./App";
 
 render(
-    <Provider store={ store }>
-        <App/>
-    </Provider>,
+    <App/>,
     window.document.querySelector(".app")
 );
