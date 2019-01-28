83f771276cc8e6f15a75cbc7816f107447104529
Pablo Lorenzo <lorenz3192@gmail.com>
2019-01-28 07:52:42 -0800

    added Dashboard

---
+++ src/js/Views/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React         from "react";
+import { Component } from "react";
+import "./dashboard.scss";
+
+class Dashboard extends Component{
+    render(){
+        return(
+            <section className="main-dashboard">
+                this is the dashboard
+            </section>
+        );
+    }
+}
+
+export default Dashboard;
