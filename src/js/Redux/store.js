9ceca6f1c35dd236b3257658691a6f96c6e36b4e
Pablo Lorenzo <lorenz3192@gmail.com>
2019-03-05 12:12:53 -0800

    fixed import issue

--- src/js/Redux/store.js
+++ src/js/Redux/store.js
@@ -2,7 +2,7 @@ import { createStore }         from "redux";
 import { applyMiddleware }     from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunkMiddleware         from "redux-thunk";
-import { rootReducer }         from "./Reducers/";
+import rootReducer             from "./Reducers/";
 
 let store;
 
