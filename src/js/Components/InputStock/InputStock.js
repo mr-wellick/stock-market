4a4cde679699fb5035279bf1c293210dade8886b
Pablo Lorenzo <lorenz3192@gmail.com>
2019-03-05 11:40:01 -0800

    input functionality working

--- src/js/Components/InputStock/InputStock.js
+++ src/js/Components/InputStock/InputStock.js
@@ -1,11 +1,29 @@
-import React     from "react";
-import PropTypes from "prop-types";
+import React        from "react";
+import { useState } from "react";
+import PropTypes    from "prop-types";
 import "./style.scss";
 
+
 function InputStock(props){
+    const [input, setInput] = useState("");
+
+    function onSubmit(event){
+        event.preventDefault();
+        event.target.children[0].value = "";
+
+        console.log(input);
+    }
+
     return(
-        <form className={ "input-form " + props.className }>
-            <input className="input is-small" type="search" />
+        <form
+            className={ "input-form " + props.className }
+            onSubmit={ onSubmit }
+        >
+            <input
+                className="input is-small"
+                type="search"
+                onChange={ (event) => setInput(event.target.value) }
+            />
         </form>
     );
 }
