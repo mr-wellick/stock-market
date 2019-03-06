0628f06c4e416a69fd0c5346d6c3b9f11a7228c8
Pablo Lorenzo <lorenz3192@gmail.com>
2019-03-06 03:15:33 -0800

    added bundle splitting

--- src/js/App.js
+++ src/js/App.js
@@ -1,15 +1,12 @@
-import React                from "react";
-import { Component }        from "react";
-import { Fragment }         from "react";
-import { hot }              from "react-hot-loader";
-import { Navigation }       from "./Components/";
-import { StockSelector }    from "./Components/";
-import { FinancialsTable }  from "./Components/";
-//import { ChartSelector }    from "./Components/";
-//import { StockDescription } from "./Components/";
-//import { StockNews }        from "./Components/";
-import { lazy }             from "react";
-import { Suspense }         from "react";
+import React               from "react";
+import { Component }       from "react";
+import { Fragment }        from "react";
+import { hot }             from "react-hot-loader";
+import { Navigation }      from "./Components/";
+import { StockSelector }   from "./Components/";
+import { FinancialsTable } from "./Components/";
+import { lazy }            from "react";
+import { Suspense }        from "react";
 import "./app.scss";
 
 const ChartSelector    = lazy(() => import("./Components/ChartSelector/ChartSelector.js"));
