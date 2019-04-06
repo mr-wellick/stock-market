300ef1044cc9eb31a2f3e9b3cfc04f8ac9e731b3
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-06 16:08:28 -0700

    ChartSelector integrated with redux

--- src/js/Components/ChartSelector/ChartSelector.js
+++ src/js/Components/ChartSelector/ChartSelector.js
@@ -1,9 +1,13 @@
-import React       from "react";
-import PropTypes   from "prop-types";
-import { connect } from "react-redux";
+import React              from "react";
+import PropTypes          from "prop-types";
+import { connect }        from "react-redux";
+import { setActiveChart } from "../../Redux/";
 import "./style.scss";
 
 function ChartSelector(props){
+    function changeChart(event) {
+        props.setActiveChart(event.target.id);
+    }
 
     if(props.data.length === 0)
         return null;
@@ -13,15 +17,15 @@ function ChartSelector(props){
             <h1 className="message">Active Chart</h1>
             <form className="chart-selector-form">
                 <div className="field-chart">
-                    <input type="radio" id="historical" name="active-chart" defaultChecked/>
+                    <input type="radio" id="historical" name="active-chart" defaultChecked onChange={ changeChart }/>
                     <label htmlFor="historical">Historical</label>
                 </div>
                 <div className="field-chart">
-                    <input type="radio" id="marketCaps" name="active-chart"/>
+                    <input type="radio" id="marketCaps" name="active-chart" onChange={ changeChart }/>
                     <label htmlFor="marketCaps">Market Cap</label>
                 </div>
                 <div className="field-chart">
-                    <input type="radio" id="financials" name="active-chart"/>
+                    <input type="radio" id="financials" name="active-chart" onChange={ changeChart }/>
                     <label htmlFor="financials">Financials</label>
                 </div>
             </form>
@@ -35,4 +39,4 @@ ChartSelector.propTypes = {
 
 const mapStateToProps = state => ({ ...state.iexDataReducer });
 
-export default connect(mapStateToProps, null)(ChartSelector);
+export default connect(mapStateToProps, { setActiveChart })(ChartSelector);
--- src/js/Redux/index.js
+++ src/js/Redux/index.js
@@ -4,3 +4,4 @@ export { default as store } from "./store.js";
 export { fetchIEXData }   from "./Actions/";
 export { setActiveIndex } from "./Actions/";
 export { deleteStock }    from "./Actions/";
+export { setActiveChart } from "./Actions/";
