328966cc1341b773610b4289f4ae78330c17303a
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-06 14:16:28 -0700

    rewrote scss for home navbar

--- src/js/Components/Logo/Logo.js
+++ src/js/Components/Logo/Logo.js
@@ -1,20 +1,15 @@
-import React     from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import "./style.scss";
 
-const Logo = (props) => {
+const Logo = () => {
     return(
-        <div className={ "logo " + props.className }>
+        <div className="logo">
             <div className="logo-block-1"></div>
             <div className="logo-block-2"></div>
             <div className="logo-block-3"></div>
             <div className="logo-block-4"></div>
         </div>
     );
-};
-
-Logo.propTypes = {
-    className: PropTypes.string
 };
 
 export default Logo;
--- src/js/Components/Navigation/Navigation.js
+++ src/js/Components/Navigation/Navigation.js
@@ -1,27 +1,53 @@
-import React          from "react";
-import { Logo }       from "../Logo/";
-import { InputStock } from "../InputStock/";
-import { Link }       from "react-router-dom";
+import React    from "react";
+import { Logo } from "../Logo/";
+import { Link } from "react-router-dom";
 import "./style.scss";
 
 function Navigation() {
+
+    function showNavbar() {
+        const navbarMenu = document.querySelector(".navbar-menu");
+        const burger     = document.querySelector(".navbar-burger");
+
+        if(!navbarMenu.className.includes("is-active"))
+        {
+            navbarMenu.className += " is-active";
+            burger.className     += " is-active";
+        }
+        else
+        {
+            navbarMenu.className = "navbar-menu";
+            burger.className     = "navbar-burger burger";
+        }
+    }
+
     return(
-        <section className="home-section">
-            <nav className="home-nav">
-                <div className="home-brand">
-                    <Link to="/">
-                        <Logo className="navbar-item"/>
+        <section className="nav-container">
+            <nav className="navbar">
+
+                <div className="navbar-brand">
+                    <Link to="/" className="navbar-item">
+                        <Logo/>
                     </Link>
+                    <a className="navbar-burger burger" onClick={ showNavbar }>
+                      <span aria-hidden="true"></span>
+                      <span aria-hidden="true"></span>
+                      <span aria-hidden="true"></span>
+                    </a>
                 </div>
-                <div className="home-menu">
-                    <div className="home-menu-start">
-                        <InputStock className="navbar-item"/>
-                    </div>
-                    <div className="home-menu-end">
+
+                <div className="navbar-menu">
+                    <div className="navbar-start">
                         <Link to="/basics" className="navbar-item">Basics</Link>
                         <Link to="/resources" className="navbar-item">Resources</Link>
                     </div>
+                    <div className="navbar-end">
+                        <div className="navbar-item">
+                            <a className="button" href="#">GitHub</a>
+                        </div>
+                    </div>
                 </div>
+
             </nav>
         </section>
     );
