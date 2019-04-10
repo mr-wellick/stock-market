e63664b1926e133fb54251a7c063a95218458c74
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-09 18:28:39 -0700

    added new hook which fetches all valid stock ticker symbols from which the user can choose from

---
+++ src/js/_hooks/useSymbols.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+function useSymbols() {
+    const [symbols, setSymbols] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.iextrading.com/1.0/ref-data/symbols")
+            .then(res => res.json())
+            .then(data => setSymbols(data));
+    },[]);
+
+    return symbols;
+}
+
+export default useSymbols;
