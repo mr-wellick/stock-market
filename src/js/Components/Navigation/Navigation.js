bfaa8743bc608c05ad94df1870d6c3003ca181a3
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-22 02:58:13 -0700

    reworked navigation to use materialize

--- src/js/Components/Navigation/Navigation.js
+++ src/js/Components/Navigation/Navigation.js
@@ -1,18 +1,28 @@
 import React from "react";
-import { Logo } from "../Logo/";
+//import { Logo } from "../Logo/";
 import { Link } from "react-router-dom";
 import "./style.scss";
 
 function Navigation() {
   return (
     <section>
-      <nav>
-        <Link to="/">
-          <Logo />
-        </Link>
-        <Link to="/basics">Basics</Link>
-        <Link to="/resources">Resources</Link>
-      </nav>
+      <div className="navbar-fixed">
+        <nav className="white z-depth-0">
+          <div className="nav-wrapper">
+            <Link to="/" className="brand-logo black-text">
+              Logo
+            </Link>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <Link to="/basics" className="black-text">Basics</Link>
+              </li>
+              <li>
+                <Link to="/resources" className="black-text">Resources</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </section>
   );
 }
