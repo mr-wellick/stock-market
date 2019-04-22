11d35971a45c148356fa0afb8f3766fa931cbaea
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-21 17:51:54 -0700

    updated build tools

--- build-utils/loadPresets.js
+++ build-utils/loadPresets.js
@@ -1,10 +1,10 @@
 let webpackMerge = require("webpack-merge");
 
-let applyPresets = (env) => {
-    let { presets }   = env;
-    let mergedPresets = [].concat(...[presets]);
-    let mergedConfigs = mergedPresets.map(
-        (presetName) => require(`./presets/webpack.${presetName}`)(env)
+let applyPresets = env => {
+    const { presets } = env;
+    const mergedPresets = [].concat(...[presets]);
+    const mergedConfigs = mergedPresets.map(presetName =>
+        require(`./presets/webpack.${presetName}`)(env)
     );
 
     return webpackMerge({}, ...mergedConfigs);
--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,75 @@
-const path           = require("path");
-const webpack        = require("webpack");
-const merge          = require("webpack-merge");
-const template       = require("html-webpack-plugin");
-const cleanBuildDir  = require("clean-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const template = require("html-webpack-plugin");
+const cleanBuildDir = require("clean-webpack-plugin");
 const purgeCSSPlugin = require("purgecss-webpack-plugin");
-const glob           = require("glob");
-
+const glob = require("glob");
 
 // Development, production, and presets
-const configType   = (env) => require(`./build-utils/webpack.${env}`)(env);
-const configPreset =          require("./build-utils/loadPresets.js");
+const configType = env =>
+  require(`./build-utils/webpack.${env}`)(env);
+const configPreset = require("./build-utils/loadPresets.js");
 
 // common webpack configuration
-module.exports = ( { mode, presets } = { mode: "production", presets: undefined } ) => {
-    return merge(
-        {
-            mode,
-            entry: path.join(__dirname, "./src/js/index.js"),
-            output:
-            {
-                path: path.join(__dirname, "build"),
-                filename: "[name].js"
-            },
-            module:
-            {
-                rules:
-                [
-                    {
-                        test: /\.(ttf)$/,
-                        use:
-                        {
-                            loader: "file-loader",
-                            options:
-                            {
-                                name: "[name].[ext]",
-                                outputPath: "fonts/"
-                            }
-                        }
-                    },
-                    {
-                        test: /\.(png|jpg|svg)$/,
-                        use:
-                        {
-                            loader: "url-loader",
-                            options: { limit: 500 }
-                        }
-                    },
-                    {
-                        test: /\.ts$/, use: [ "ts-loader" ]
-                    },
-                    {
-                        test: /\.js$/,
-                        loader: "babel-loader",
-                        query:
-                        {
-                            presets: [ "@babel/preset-react", "@babel/preset-env" ]
-                        }
-                    }
-                ]
-            },
-            plugins:
-            [
-                new template({ template: "src/index.html", favicon: "src/_assets/logo.png" }),
-                new webpack.ProgressPlugin(),
-                new cleanBuildDir(["build"]),
-                new purgeCSSPlugin({
-                    paths: glob.sync(`${path.join(__dirname, "./src/")}/**/*`,
-                    { nodir: true })
-                })
-            ]
-        },
-        configType(mode),
-        presets !== undefined ? configPreset({ mode, presets }) : null
-    );
+module.exports = (
+  { mode, presets } = { mode: "production", presets: undefined }
+) => {
+  return merge(
+    {
+      mode,
+      entry: path.join(__dirname, "./src/js/index.js"),
+      output: {
+        path: path.join(__dirname, "build"),
+        filename: "[name].js"
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(ttf)$/,
+            use: {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]",
+                outputPath: "fonts/"
+              }
+            }
+          },
+          {
+            test: /\.(png|jpg|svg)$/,
+            use: {
+              loader: "url-loader",
+              options: { limit: 500 }
+            }
+          },
+          {
+            test: /\.ts$/,
+            use: ["ts-loader"]
+          },
+          {
+            test: /\.js$/,
+            loader: "babel-loader",
+            query: {
+              presets: ["@babel/preset-react", "@babel/preset-env"]
+            }
+          }
+        ]
+      },
+      plugins: [
+        new template({
+          template: "src/index.html",
+          favicon: "src/_assets/logo.png"
+        }),
+        new webpack.ProgressPlugin(),
+        new cleanBuildDir(),
+        new purgeCSSPlugin({
+          paths: glob.sync(`${path.join(__dirname, "./src/")}/**/*`, {
+            nodir: true
+          })
+        })
+      ]
+    },
+    configType(mode),
+    presets !== undefined ? configPreset({ mode, presets }) : null
+  );
 };
