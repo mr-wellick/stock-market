9ac48b23519a8f4885bdf51c6dcfdf7d2f19f7c3
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-22 02:57:55 -0700

    commented out css and commented out graphs & stock data

--- src/js/Views/Home/Home.js
+++ src/js/Views/Home/Home.js
@@ -1,49 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { StockSelector } from "../../Components/";
+//import { StockSelector } from "../../Components/";
 import { connect } from "react-redux";
 import { InputStock } from "../../Components/";
 //import { FinancialsTable }  from "../../Components/";
 //import { StockDescription } from "../../Components/";
-import { ChartSelector } from "../../Components/";
-import { ActiveChart } from "../../Components/";
-import { StockHeader } from "../../Components/";
+//import { ChartSelector } from "../../Components/";
+//import { ActiveChart } from "../../Components/";
+//import { StockHeader } from "../../Components/";
 import "./style.scss";
 
 function Home(props) {
-  function toggleSidebar() {
-    const sidebar = document.querySelector(".sidebar");
-    const content = document.querySelector(".sidebar-content");
-
-    if (!sidebar.className.includes(" is-open")) {
-      sidebar.className += " is-open";
-      content.className += " is-full-width";
-    } else {
-      sidebar.className = "sidebar";
-      content.className = "sidebar-content";
-    }
-  }
 
   return (
-    <section className="sidebar-container">
-      <div className="sidebar">
-        <div className="sidebar-toggler" onClick={toggleSidebar}>
-          &gt;&gt;
-        </div>
+    <section>
+      <div className="sidenav sidenav-fixed">
         <InputStock />
-        <hr style={{ margin: "0" }} />
-        <StockSelector />
-        <ChartSelector />
+        {/*
+          <StockSelector />
+          <ChartSelector />
+        */}
       </div>
+      {/*
       <div className="sidebar-content">
         <StockHeader />
         <ActiveChart />
-        {/*
-                <FinancialsTable/>
-                <StockDescription/>
-                <StockNews/>
-                */}
+        <FinancialsTable/>
+        <StockDescription/>
+        <StockNews/>
       </div>
+      */}
     </section>
   );
 }
