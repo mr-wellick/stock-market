4f50ad30f9ada1956b0c1c4d5a83dcbf95466632
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-21 17:51:40 -0700

    removed react-hot-loader for now

--- src/js/App.js
+++ src/js/App.js
@@ -1,41 +1,33 @@
 import "./app.scss";
-import React          from "react";
-import { Component }  from "react";
-import { Fragment }   from "react";
-import { hot }        from "react-hot-loader";
+import React from "react";
+import { Component } from "react";
+import { Fragment } from "react";
 import { HashRouter } from "react-router-dom";
-import { Switch }     from "react-router-dom";
-//import { Route }      from "react-router-dom";
+import { Switch } from "react-router-dom";
+//import { Route } from "react-router-dom";
 import { Navigation } from "./Components/";
-//import { Home }       from "./Views/";
+//import { Home } from "./Views/";
 //import { Basics }     from "./Views/";
 //import { Resources }  from "./Views/";
 
 // react-hot-loader complains when using plain functions
 class App extends Component {
-    render(){
-        return(
-            <HashRouter>
-                <Fragment>
-                    <Navigation/>
-                    <Switch>
-                        {/*
-                        <Route exact path="/" component={ Home }></Route>
-                        <route exact path="/basics" component={ basics }></route>
-                        <route exact path="/resources" component={ resources }></route>
-                        */}
-                    </Switch>
-                </Fragment>
-            </HashRouter>
-        );
-    }
+  render() {
+    return (
+      <HashRouter>
+        <Fragment>
+          <Navigation />
+          <Switch>
+            {/*
+              <Route exact path="/" component={Home} />
+              <Route exact path="/basics" component={ basics }></route>
+              <Route exact path="/resources" component={ resources }></route>
+            */}
+          </Switch>
+        </Fragment>
+      </HashRouter>
+    );
+  }
 }
 
-let Application;
-
-if(process.env.NODE_ENV === "development")
-    Application = hot(module)(App);
-else
-    Application = App;
-
-export default Application;
+export default App;
