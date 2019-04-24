3d3164137e40e121faa2f49f531c14789f740a5d
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-24 08:07:21 -0700

    rewrote markup/css to use materialize

--- src/js/Components/StockHeader/StockHeader.js
+++ src/js/Components/StockHeader/StockHeader.js
@@ -4,24 +4,29 @@ import { connect } from "react-redux";
 import "./style.scss";
 
 function StockHeader(props) {
-  if (props.data.length === 0) return null;
+  if (props.data.length === 0) {
+    return null;
+  }
 
   const { data, activeIndex } = props;
-
   const isDown = data[activeIndex].quote.change < 0 ? "is-down" : "is-up";
 
   return (
-    <div className="stock-header-container">
-      <p>
-        <span className="company-name">{data[activeIndex].company.companyName}</span>
-        <span className="company-symbol">[{data[activeIndex].company.symbol}]</span>
-      </p>
-      <p>
-        <span className="company-close">{data[activeIndex].quote.close}</span>
-        <span className={"company-change " + isDown}>
-          {data[activeIndex].quote.change}({data[activeIndex].quote.changePercent})
-        </span>
-      </p>
+    <div className="row">
+      <div className="col s12">
+        <div className="card-pannel">
+          <p>
+            <span className="company-name">{data[activeIndex].company.companyName}</span>
+            <span className="company-symbol">[{data[activeIndex].company.symbol}]</span>
+          </p>
+          <p>
+            <span className="company-close">{data[activeIndex].quote.close}</span>
+            <span className={"company-change " + isDown}>
+              {data[activeIndex].quote.change}({data[activeIndex].quote.changePercent})
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
