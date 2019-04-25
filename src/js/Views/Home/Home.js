d829ce31e02e88ca12ca0cf5db384166a09f1801
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-24 23:49:16 -0700

    added chart selector

--- src/js/Views/Home/Home.js
+++ src/js/Views/Home/Home.js
@@ -5,31 +5,29 @@ import { connect } from "react-redux";
 import { InputStock } from "../../Components/";
 //import { FinancialsTable }  from "../../Components/";
 //import { StockDescription } from "../../Components/";
-//import { ChartSelector } from "../../Components/";
+import { ChartSelector } from "../../Components/";
 import { ActiveChart } from "../../Components/";
 import { StockHeader } from "../../Components/";
 import "./style.scss";
 
 function Home() {
   return (
-    <>
-      <div className="divider" />
-      <section>
-        <div className="row">
-          {/* SIDEBAR */}
-          <div className="main-sidebar col s3">
-            <InputStock />
-            <StockSelector/>
-          </div>
-
-          {/* MAIN CONTENT */}
-          <div className="main-content col s9 grey lighten-5">
-            <StockHeader />
-            <ActiveChart />
-          </div>
+    <section>
+      <div className="row">
+        {/* SIDEBAR */}
+        <div className="main-sidebar col s3">
+          <InputStock />
+          <StockSelector />
+          <ChartSelector />
         </div>
-      </section>
-    </>
+
+        {/* MAIN CONTENT */}
+        <div className="main-content col s9 grey lighten-5">
+          <StockHeader />
+          <ActiveChart />
+        </div>
+      </div>
+    </section>
   );
 }
 
