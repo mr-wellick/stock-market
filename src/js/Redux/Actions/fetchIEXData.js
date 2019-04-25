980581b7a65451280b317b32f94e64bc15210ece
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-25 10:34:07 -0700

    added logo to api request

--- src/js/Redux/Actions/fetchIEXData.js
+++ src/js/Redux/Actions/fetchIEXData.js
@@ -8,7 +8,7 @@ const fetchIEXData = function(stockName) {
     dispatch({ type: IS_FETCHING, isFetching: true });
 
     const response = await fetch(
-      `https://api.iextrading.com/1.0/stock/${stockName}/batch?types=relevant,stats,company,financials,news,quote,chart&range=5y`
+      `https://api.iextrading.com/1.0/stock/${stockName}/batch?types=logo,relevant,stats,company,financials,news,quote,chart&range=5y`
     );
 
     try {
