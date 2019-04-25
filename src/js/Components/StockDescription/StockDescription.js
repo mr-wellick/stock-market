4c581fd949c48fde75edca9ab7f335fd65595c88
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-25 11:07:58 -0700

    rewrote html markup

--- src/js/Components/StockDescription/StockDescription.js
+++ src/js/Components/StockDescription/StockDescription.js
@@ -9,29 +9,27 @@ function StockDescription(props) {
   if (data.length === 0) return null;
 
   return (
-    <div className="row">
-      <div className="col s12">
-        <h2 className="header">
-          CEO: {data[activeIndex].company.CEO ? data[activeIndex].company.CEO : "No CEO"}
-        </h2>
-        <div className="card horizontal">
-          <div className="card-image valign-wrapper">
-            <img src={data[activeIndex].logo.url} alt="company logo" />
+    <div> 
+      <h4 className="header">
+        CEO: {data[activeIndex].company.CEO ? data[activeIndex].company.CEO : "No CEO"}
+      </h4>
+      <div className="card horizontal">
+        <div className="card-image valign-wrapper">
+          <img src={data[activeIndex].logo.url} alt="company logo" />
+        </div>
+        <div className="card-stacked">
+          <div className="card-content">
+            <p className="">{data[activeIndex].company.description}</p>
           </div>
-          <div className="card-stacked">
-            <div className="card-content">
-              <p className="">{data[activeIndex].company.description}</p>
-            </div>
-            <div className="card-action">
-              <a
-                className="blue-text"
-                href={data[activeIndex].company.website}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {data[activeIndex].company.symbol}
-              </a>
-            </div>
+          <div className="card-action">
+            <a
+              className="blue-text"
+              href={data[activeIndex].company.website}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {data[activeIndex].company.symbol}
+            </a>
           </div>
         </div>
       </div>
