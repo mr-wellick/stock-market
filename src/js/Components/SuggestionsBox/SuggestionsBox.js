61984761017aefb2ba4c843b40884ef49d76af79
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-02 12:28:00 -0700

    Suggestions box base functionality complete.
    Added css file to correctly position suggestions box when it appears
    as the user is typing some input. Also, added more conditional
    checking.

--- src/js/Components/SuggestionsBox/SuggestionsBox.js
+++ src/js/Components/SuggestionsBox/SuggestionsBox.js
@@ -1,17 +1,27 @@
 import React from "react";
+import "./style.scss";
 
 const SuggestionsBox = props => {
-  let matches = [];
+  let matches;
 
   if (props.matches !== null) {
     matches = props.matches.length < 200 ? props.matches : props.matches.slice(0, 200);
   }
 
+  if (matches === undefined || matches === null || matches.length === 0) {
+    return null;
+  }
+
   return (
-    <div>
-      {matches.map(symbol => (
-        <div key={symbol}>{symbol}</div>
-      ))}
+    <div className="suggestions-box">
+      <form>
+        {matches.map(symbol => (
+          <div key={symbol}>
+            <input type="radio" id={symbol} />
+            <label htmlFor={symbol}>{symbol}</label>
+          </div>
+        ))}
+      </form>
     </div>
   );
 };
