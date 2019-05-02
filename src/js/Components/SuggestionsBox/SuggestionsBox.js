b9f1bbcc71a5ce6e9df7e9689908ca2837d6b1e8
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-02 11:43:27 -0700

    Added conditional logic
    Added an if statement to check whether or not the passed in
    prop, matches, is null.

--- src/js/Components/SuggestionsBox/SuggestionsBox.js
+++ src/js/Components/SuggestionsBox/SuggestionsBox.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SuggestionsBox = props => {
-  const matches = props.matches.length < 200 ? props.matches : props.matches.slice(0, 200);
+  let matches = [];
+
+  if (props.matches !== null) {
+    matches = props.matches.length < 200 ? props.matches : props.matches.slice(0, 200);
+  }
 
   return (
     <div>
