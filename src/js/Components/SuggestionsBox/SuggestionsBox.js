1ba0858e4980bf27768b79b2b0bf415e314c25cf
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-02 11:29:16 -0700

    Wrote minimum logic required to display suggestions
    The component now successfully displays all possible stock suggestions
    as the user enters their input.

--- src/js/Components/SuggestionsBox/SuggestionsBox.js
+++ src/js/Components/SuggestionsBox/SuggestionsBox.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const SuggestionsBox = () => {
-  return <div>this will be a suggestions box</div>;
+const SuggestionsBox = props => {
+  const matches = props.matches.length < 200 ? props.matches : props.matches.slice(0, 200);
+
+  return (
+    <div>
+      {matches.map(symbol => (
+        <div key={symbol}>{symbol}</div>
+      ))}
+    </div>
+  );
 };
 
 export default SuggestionsBox;
