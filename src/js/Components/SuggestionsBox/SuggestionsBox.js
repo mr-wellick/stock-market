f4189f0cc3a0448c3aa558dbce5ff8978b85200c
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-05 00:45:12 -0700

    Completed SuggestionsBox functionality
    SugesstionsBox functionality is complete. However, now we need to lazy
    the suggestions displayed to the user!

--- src/js/Components/SuggestionsBox/SuggestionsBox.js
+++ src/js/Components/SuggestionsBox/SuggestionsBox.js
@@ -1,31 +1,29 @@
 import React from "react";
 import "./style.scss";
 
-const SuggestionsBox = props => {
-  let matches = [];
-
-  // this is here so we can implement base functionality. Loading more than 200 entries is SLOW!
-  // until we implement "load more on scroll" functionality, we need this here.
-  if (props.matches !== null) {
-    matches = props.matches.length < 200 ? props.matches : props.matches.slice(0, 200);
-  }
-
-  if (matches.length === 0) {
+function SuggestionsBox(props) {
+  if (props.matches.length === 0) {
     return null;
   }
 
   return (
-    <div className="suggestions-box">
-      <form>
-        {matches.map(symbol => (
-          <div key={symbol}>
-            <input type="radio" id={symbol} />
-            <label htmlFor={symbol}>{symbol}</label>
-          </div>
-        ))}
-      </form>
-    </div>
+    <form className="suggestions-box">
+      {props.matches.map(symbol => (
+        <p key={symbol}>
+          <label>
+            <input
+              name="stocks"
+              className="with-gap"
+              type="radio"
+              onChange={props.onSelectEntry}
+              data-stock-name={symbol}
+            />
+            <span>{symbol}</span>
+          </label>
+        </p>
+      ))}
+    </form>
   );
-};
+}
 
 export default SuggestionsBox;
