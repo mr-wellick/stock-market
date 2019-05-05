4e3497a7c84aa22c9d87c42996d7e2bcf9dded26
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-04 20:11:55 -0700

    Simplified conditional checking
    Only using less than 200 entries since loading more than 200 items is
    SLOW! This will be removed once we have "load more on scroll"
    functionality.
    
        -Conditional checks were simplified.

--- src/js/Components/SuggestionsBox/SuggestionsBox.js
+++ src/js/Components/SuggestionsBox/SuggestionsBox.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./style.scss";
 
 const SuggestionsBox = props => {
-  let matches;
+  let matches = [];
 
+  // this is here so we can implement base functionality. Loading more than 200 entries is SLOW!
+  // until we implement "load more on scroll" functionality, we need this here.
   if (props.matches !== null) {
     matches = props.matches.length < 200 ? props.matches : props.matches.slice(0, 200);
   }
 
-  if (matches === undefined || matches === null || matches.length === 0) {
+  if (matches.length === 0) {
     return null;
   }
 
