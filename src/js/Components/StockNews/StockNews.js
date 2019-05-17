0b5c4d42e8f674284db62715e7c02c5bbaff9a78
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-17 15:45:54 -0700

    retructured News html

--- src/js/Components/StockNews/StockNews.js
+++ src/js/Components/StockNews/StockNews.js
@@ -1,44 +1,42 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import "./style.scss";
 
 function StockNews(props) {
   const { data, activeIndex } = props;
 
-  if (data.length === 0) return null;
+  if (data.length === 0) {
+    return null;
+  }
 
   return (
-    <section>
-      <h3>News</h3>
-      {data[activeIndex].news.map(stock => (
-        <div key={stock.url} className="row">
-          <div className="col s12">
-            <div className="card blue-grey darken-1">
-              <div className="card-content white-text">
-                <span className="card-title">
-                  {`${stock.source} -- ${new Date(stock.datetime).toLocaleString()}`}
-                </span>
-                <span className="card-title">{stock.headline}</span>
-                <p>{stock.summary}</p>
+    <div className="news-container row">
+      <h3 style={{ paddingLeft: "10px" }}>News</h3>
+      {data[activeIndex].news.map(article => (
+        <div key={article.url} className="col s12">
+          <div className="card horizontal hoverable">
+            <div className="image-container card-image">
+              <img className="responsive-image" src={article.image} alt="not-available" />
+            </div>
+            <div className="card-stacked">
+              <div className="card-content">
+                <h6 id="article-headline">
+                  <b>{article.headline}</b>
+                </h6>
+                <p className="article-summary">{article.summary.substring(0, 250)}...</p>
               </div>
               <div className="card-action">
-                <a rel="noopener noreferrer" target="_blank" href={stock.url}>
-                  Read More
+                <a href={article.url}>
+                  {`${article.source} | ${new Date(article.datetime).toLocaleString()}`}
                 </a>
               </div>
             </div>
           </div>
         </div>
       ))}
-    </section>
+    </div>
   );
 }
-
-StockNews.propTypes = {
-  data: PropTypes.array,
-  activeIndex: PropTypes.number
-};
 
 const mapStateToProps = state => ({ ...state.iexDataReducer });
 
