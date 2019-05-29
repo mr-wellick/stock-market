7f06189b2a35be5dacb65d9e8f99b32c55631296
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-29 06:16:21 -0700

    changed to arrow func

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,5 @@
 import "./app.scss";
 import React from "react";
-import { Component } from "react";
 import { Fragment } from "react";
 import { HashRouter } from "react-router-dom";
 import { Switch } from "react-router-dom";
@@ -11,30 +10,28 @@ import { SmallLoader } from "./Components/";
 import { Home } from "./Views/";
 //import { Contact } from "./Views/";
 
-class App extends Component {
-  render() {
-    return (
-      <HashRouter>
-        <div className="main-container row">
-          <div className="main-sidebar-container col s3">
-            <SmallLoader />
-            <Navigation />
-          </div>
-          <div className="main-content-container col s9 right">
-            <Fragment>
-              <Switch>
-                <Route exact path="/" component={Home} />
-                {/*
+const App = () => {
+  return (
+    <HashRouter>
+      <div className="main-container row">
+        <div className="main-sidebar-container col s3">
+          <SmallLoader />
+          <Navigation />
+        </div>
+        <div className="main-content-container col s9 right">
+          <Fragment>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              {/*
                   <Route exact path="/contact" component={Contact} />
                   <Route exact path="/basics" component={ Basics }/>
                 */}
-              </Switch>
-            </Fragment>
-          </div>
+            </Switch>
+          </Fragment>
         </div>
-      </HashRouter>
-    );
-  }
-}
+      </div>
+    </HashRouter>
+  );
+};
 
 export default hot(App);
