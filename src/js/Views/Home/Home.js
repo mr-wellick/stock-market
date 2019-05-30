b483e3fb9b01c8fcdf35bb29cac45d4be1ab1cec
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-29 17:47:22 -0700

    added footer to home page

--- src/js/Views/Home/Home.js
+++ src/js/Views/Home/Home.js
@@ -1,25 +1,23 @@
 import React from "react";
-import { connect } from "react-redux";
 import { InputStock } from "../../Components/";
 import { StockDescription } from "../../Components/";
 import { ActiveChart } from "../../Components/";
 import { StockNews } from "../../Components/";
+import { FinancialsTable } from "../../Components/";
+import { Footer } from "../../Components/";
 import "./style.scss";
 
-const Home = props => {
+const Home = () => {
   return (
-    <section className={props.data.length === 0 ? "fill-viewport" : ""}>
+    <section>
       <InputStock />
+      <FinancialsTable />
       <ActiveChart />
       <StockDescription />
       <StockNews />
+      <Footer />
     </section>
   );
 };
 
-const mapStateToProps = state => ({ ...state.iexDataReducer });
-
-export default connect(
-  mapStateToProps,
-  null
-)(Home);
+export default Home;
