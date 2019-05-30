e34b52fb34fede9e7072ff4b314906fac6d1f968
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-29 17:46:09 -0700

    added footer component

--- src/js/Components/Footer/Footer.js
+++ src/js/Components/Footer/Footer.js
@@ -1,32 +1,31 @@
 import React from "react";
+import { Logo } from "../Logo/";
 import "./style.scss";
 
 const Footer = () => {
   return (
     <footer className="main-footer">
-      <div className="main-footer__container">
-        <p>
-          <span>
-            Developed by&nbsp;
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/Ll2NZ/securities-app"
-            >
-              Ll2NZ
-            </a>
-          </span>
-          <span className="dev-info"> | </span>
-          <span className="dev-info">Data provided for free by&nbsp;</span>
+      <div className="developed-by">
+        <Logo />
+        <span className="grey-text">
+          Developed by&nbsp;
           <a
+            className="text-lighten-4"
             target="_blank"
             rel="noopener noreferrer"
-            href="https://iextrading.com/developer/"
-            className="dev-info"
+            href="https://github.com/Ll2NZ"
           >
+            LRNZ
+          </a>
+        </span>
+      </div>
+      <div>
+        <span className="grey-text">
+          Data provided by&nbsp;
+          <a target="_blank" rel="noopener noreferrer" href="https://iexcloud.io/docs/api/">
             IEX
           </a>
-        </p>
+        </span>
       </div>
     </footer>
   );
