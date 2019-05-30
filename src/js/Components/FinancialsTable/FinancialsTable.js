ddac0190a69af38d09f03afe193f6dbd84346dec
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-29 17:46:53 -0700

    added financials-table component

--- src/js/Components/FinancialsTable/FinancialsTable.js
+++ src/js/Components/FinancialsTable/FinancialsTable.js
@@ -9,30 +9,31 @@ const FinancialsTable = props => {
   }
 
   const { quote, stats } = props.data[props.activeIndex];
+  const stockTrend = change => (change < 0 ? "is-down" : "is-up");
 
   return (
-    <div className="card">
-      {
-        <table>
-          <thead>
-            <tr>
-              <th>Symbol</th>
-              <th>Close</th>
-              <th>SO</th>
-              <th>MC</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{!quote.symbol ? "NA" : quote.symbol}</td>
-              <td>{!quote.close ? "NA" : quote.close}</td>
-              <td>{!stats.sharesOutstanding ? "NA" : format(".2s")(stats.sharesOutstanding)}</td>
-              <td>{!stats.marketcap ? "NA" : format(".2s")(stats.marketcap)}</td>
-            </tr>
-          </tbody>
-        </table>
-      }
-    </div>
+    <table className="centered">
+      <thead>
+        <tr>
+          <th>Symbol</th>
+          <th>Close</th>
+          <th>Shares Outstanding</th>
+          <th>Market Cap</th>
+          <th>Week 52 High</th>
+          <th>Week 52 Low</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{!quote.symbol ? "-" : quote.symbol}</td>
+          <td className={stockTrend(quote.change)}>{!quote.close ? "-" : quote.close}</td>
+          <td>{!stats.sharesOutstanding ? "-" : format(".2s")(stats.sharesOutstanding)}</td>
+          <td>{!stats.marketcap ? "-" : format(".2s")(stats.marketcap)}</td>
+          <td>{!quote.week52High ? "-" : quote.week52High}</td>
+          <td>{!quote.week52Low ? "-" : quote.week52Low}</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
