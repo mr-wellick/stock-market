cb225c5cf73079a2c78c6d99e0b6ec7f777b6c70
Pablo Lorenzo <lorenz3192@gmail.com>
2019-07-12 03:01:27 -0700

    File rename

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: "jest-environment-node",
+  collectCoverageFrom: ["**/src/js/**/*"],
+  coverageThreshold: {
+    global: {
+      statements: 50,
+      branches: 50,
+      functions: 50,
+      lines: 100
+    }
+  }
+};
