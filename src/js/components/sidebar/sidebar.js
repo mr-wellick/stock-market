8cbb5073e41df435d869ec4ae3c583c15981d934
Pablo Lorenzo <lorenz3192@gmail.com>
2019-07-16 13:54:24 -0700

    Sidebar component basic outline complete
      - Build out sidebar header
      - Build out sidebar contents
      - Build out sidebar footer

--- src/js/components/sidebar/sidebar.js
+++ src/js/components/sidebar/sidebar.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { LrnzLogo } from '../LrnzLogo/';
 import './style.scss';
 
 const Sidebar = () => {
   return (
     <section className="sidebar">
-      <div className="sidebar-logo">
-        <a href="lrnz">Logo</a>
+      <div className="sidebar-logo-container">
+        <div className="sidebar-logo">
+          <LrnzLogo />
+        </div>
+        <div className="sidebar-brand">
+          <h3>LRNZ</h3>
+        </div>
       </div>
       <div className="sidebar-contents">
         <ul className="sidebar-list">
           <li className="sidebar-list__item">Dashboard</li>
         </ul>
+      </div>
+      <div className="sidebar-footer">
+        <div className="sidebar-footer-icon">
+          <svg width="100%" height="100%" viewBox="0 0 18 18">
+            <path d="M5.46 8.846l3.444-3.442-1.058-1.058-4.5 4.5 4.5 4.5 1.058-1.057L5.46 8.84zm7.194 4.5v-9h-1.5v9h1.5z" />
+          </svg>
+        </div>
       </div>
     </section>
   );
