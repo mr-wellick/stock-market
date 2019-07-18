52af35a62e794ddb9d8c771c58b082f6a70c70ff
Pablo Lorenzo <lorenz3192@gmail.com>
2019-07-17 19:37:58 -0700

    Button component complete

--- src/js/components/button/button.js
+++ src/js/components/button/button.js
@@ -3,11 +3,13 @@ import './style.scss';
 
 const Button = () => {
   return (
-    <span className="dot">
-      <svg width="20px" height="20px">
-        <path d="M5.46 8.846l3.444-3.442-1.058-1.058-4.5 4.5 4.5 4.5 1.058-1.057L5.46 8.84zm7.194 4.5v-9h-1.5v9h1.5z" />
-      </svg>
-    </span>
+    <div>
+      <button className="button">
+        <svg width="40px" height="40px" viewBox="-3 -10 40 40">
+          <path d="M5.46 8.846l3.444-3.442-1.058-1.058-4.5 4.5 4.5 4.5 1.058-1.057L5.46 8.84zm7.194 4.5v-9h-1.5v9h1.5z" />
+        </svg>
+      </button>
+    </div>
   );
 };
 
