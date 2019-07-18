ef2b4f7d9056982a9617554422dd8556ba8b0fea
Pablo Lorenzo <lorenz3192@gmail.com>
2019-07-17 19:37:19 -0700

    Added two more items to navigation list

--- src/js/components/navigation/navigation.js
+++ src/js/components/navigation/navigation.js
@@ -17,6 +17,30 @@ const Navigation = () => {
             </a>
           </div>
         </li>
+        <li className="nav-item">
+          <div className="nav-item__container">
+            <i className="nav-item__icon">
+              <svg height="18px" width="18px">
+                <path d="M17 9.008l-3.363-3.363-1.883 1.883 1.48 1.48-1.48 1.48 1.883 1.882L17 9.008zM8.992 1l3.363 3.363-1.883 1.883-1.48-1.48-1.48 1.48L5.63 4.363 8.992 1zm.016 16l-3.363-3.363 1.883-1.883 1.48 1.48 1.48-1.48 1.882 1.883L9.008 17zM1 8.992l3.363 3.363 1.883-1.883-1.48-1.48 1.48-1.48L4.363 5.63 1 8.992zM9.008 7.32l1.688 1.688-1.688 1.688-1.69-1.688 1.69-1.69z" />
+              </svg>
+            </i>
+            <a className="nav-item__link" href="/financials">
+              Financials
+            </a>
+          </div>
+        </li>
+        <li className="nav-item">
+          <div className="nav-item__container">
+            <i className="nav-item__icon">
+              <svg height="18px" width="18px">
+                <path d="M17 9.008l-3.363-3.363-1.883 1.883 1.48 1.48-1.48 1.48 1.883 1.882L17 9.008zM8.992 1l3.363 3.363-1.883 1.883-1.48-1.48-1.48 1.48L5.63 4.363 8.992 1zm.016 16l-3.363-3.363 1.883-1.883 1.48 1.48 1.48-1.48 1.882 1.883L9.008 17zM1 8.992l3.363 3.363 1.883-1.883-1.48-1.48 1.48-1.48L4.363 5.63 1 8.992zM9.008 7.32l1.688 1.688-1.688 1.688-1.69-1.688 1.69-1.69z" />
+              </svg>
+            </i>
+            <a className="nav-item__link" href="/notes">
+              Notes
+            </a>
+          </div>
+        </li>
       </ul>
     </nav>
   );
