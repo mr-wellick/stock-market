03acbd30eed48d960c1def35ee2bf4f982e3c402
Pablo Lorenzo <lorenz3192@gmail.com>
2019-07-19 11:43:59 -0700

    updated imports

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,13 @@
 import './app.scss';
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { NavBar } from './components/';
 import { Sidebar } from './components/';
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Sidebar />
     </>
   );
