85afca8d8a00a4dbcce9d2d99783cb657483e4db
Pablo Lorenzo <lorenz3192@gmail.com>
2019-07-19 15:40:02 -0700

    completed base nav-bar layout

--- src/js/components/nav-bar/nav-bar.js
+++ src/js/components/nav-bar/nav-bar.js
@@ -6,19 +6,20 @@ const NavBar = () => {
     <section className="nav-bar-section">
       <header className="nav-bar-header">
         <div className="nav-bar__left">
-          <button className="button">
+          <button className="button" id="nav-button">
             <svg height="24px" width="24px">
               <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
             </svg>
           </button>
         </div>
         <div className="nav-bar__middle">
-          <form>
-            <input type="text" />
+          <form className="nav-form">
+            <input className="nav-form__input" type="text" />
           </form>
         </div>
         <div className="nav-bar__right">
-          <button className="button">x</button>
+          <button>login</button>
+          <button>register</button>
         </div>
       </header>
     </section>
