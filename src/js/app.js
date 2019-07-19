ce811339f4ab8ae0e81a9abf13a953f01b2af66c
Pablo Lorenzo <lorenz3192@gmail.com>
2019-07-19 16:37:00 -0700

    main-content container prototype complete

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,21 @@ const App = () => {
     <>
       <NavBar />
       <Sidebar />
+
+      {/* will remove. just prototyping main container that will hold all of our content */}
+      <section style={{ paddingTop: '50px', marginLeft: 'auto', width: 'calc(100% - 256px)' }}>
+        <div
+          style={{
+            height: '48px',
+            borderBottom: '1.1px solid rgba(0, 0, 0, 0.12)',
+            display: 'flex',
+            alignItems: 'center'
+          }}
+        >
+          summary
+        </div>
+        <div style={{ padding: '24px' }}>main content</div>
+      </section>
     </>
   );
 };
