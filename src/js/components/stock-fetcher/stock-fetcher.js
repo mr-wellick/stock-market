781e6d813f5514800128d9b713b49b66bc3bd6f9
Pablo Lorenzo <lorenz3192@gmail.com>
2019-07-21 16:04:16 -0700

    incorporated FETCH_IEX_DATA into stock-fetcher

--- src/js/components/stock-fetcher/stock-fetcher.js
+++ src/js/components/stock-fetcher/stock-fetcher.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
+import { connect } from 'react-redux';
+import { fetchIEXData } from '../../redux/';
+import { validate } from '../../_utilities/';
 import './style.scss';
 
-const StockFetcher = () => {
+const StockFetcher = props => {
+  const [state, setState] = useState('');
+
+  const onSubmit = event => {
+    event.preventDefault();
+    const validStockName = validate(state);
+
+    if (validStockName) {
+      props.fetchIEXData(state);
+    } else {
+      console.log('wooops');
+    }
+
+    document.querySelector('.stock-fetcher__input').value = '';
+  };
+
   return (
-    <form className="stock-fetcher__form">
-      <input className="stock-fetcher__input" type="text" />
+    <form className="stock-fetcher__form" autoComplete="off" onSubmit={onSubmit}>
+      <input
+        onChange={event => setState(event.target.value)}
+        className="stock-fetcher__input"
+        type="text"
+      />
     </form>
   );
 };
 
-export default StockFetcher;
+export default connect(
+  null,
+  { fetchIEXData }
+)(StockFetcher);
