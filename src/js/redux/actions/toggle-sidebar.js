33f68c92f11770e0bf91bc2efe965726d109164b
Pablo Lorenzo <lorenz3192@gmail.com>
2019-07-28 14:03:07 -0700

    refactor

--- src/js/redux/actions/toggle-sidebar.js
+++ src/js/redux/actions/toggle-sidebar.js
@@ -1,15 +1,19 @@
 import { SIDEBAR_TOGGLED } from '../constants/';
 import store from '../store.js';
 
-const toggleSidebar = function() {
-  return dispatch => {
-    const { className } = store.getState().uiReducer;
+const toggleSidebar = () => {
+  const { className } = store.getState().uiReducer;
 
-    if (className === '') {
-      dispatch({ type: SIDEBAR_TOGGLED, payload: { className: 'toggled' } });
-    } else {
-      dispatch({ type: SIDEBAR_TOGGLED, payload: { className: '' } });
-    }
+  if (className === '') {
+    return {
+      type: SIDEBAR_TOGGLED,
+      payload: { className: 'toggled' }
+    };
+  }
+
+  return {
+    type: SIDEBAR_TOGGLED,
+    payload: { className: '' }
   };
 };
 
