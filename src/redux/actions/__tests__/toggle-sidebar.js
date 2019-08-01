8aa71096e944827f25d7ffb7a7d4e3a4c895e7c2
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-01 00:33:06 -0700

    typo

--- src/redux/actions/__tests__/toggle-sidebar.js
+++ src/redux/actions/__tests__/toggle-sidebar.js
@@ -1,7 +1,7 @@
 import toggleSidebar from '../toggle-sidebar.js';
 import { SIDEBAR_TOGGLED } from '../../constants/';
 
-// this is a bit ticky since our toggleSidebar method will return either
+// this is a bit tricky since our toggleSidebar method will return either
 // an empty string '' or the following string 'toggled'
 // to test this works correctly we need to dispatch an action of type SIDEBAR_TOGGLED
 test('Action creator toggling main sidebar', () => {
