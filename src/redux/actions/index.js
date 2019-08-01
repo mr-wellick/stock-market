1521412b9c8b7468efad3c4897ec79d5bb51f111
Pablo Lorenzo <lorenz3192@gmail.com>
2019-07-31 22:54:41 -0700

    removed imports for old fetch-iex-data action creator

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,3 +1,2 @@
 export { default as toggleSidebar } from './toggle-sidebar.js';
-export { default as fetchIEXData } from './fetch-iex-data.js';
 export { default as setActiveStock } from './set-active-stock.js';
