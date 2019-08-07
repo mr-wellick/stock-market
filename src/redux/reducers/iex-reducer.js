9c3fea53901979d60486fc04c6860600119a7d3a
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-07 02:37:47 -0700

    added mock-data directory

--- src/redux/reducers/iex-reducer.js
+++ src/redux/reducers/iex-reducer.js
@@ -1,7 +1,7 @@
 import { FETCH_IEX_DATA } from '../constants/';
 import { FETCH_ERROR } from '../constants/';
 import { SET_ACTIVE_STOCK } from '../constants/';
-import { TSLA } from './tsla-data.js';
+import { TSLA } from '../../mock-data/tsla.js';
 
 function iexReducer(state = { data: { TSLA }, error: '', activeStock: 'TSLA' }, action) {
   if (action.type === FETCH_IEX_DATA) {
