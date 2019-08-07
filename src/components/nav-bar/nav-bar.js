dc5f580812a60137ff72dc3253a08db030de1294
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-06 22:24:28 -0700

    nav-bar css styles complete

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -7,15 +7,15 @@ const NavBar = () => {
   return (
     <header className="main-header">
       <nav className="main-nav">
-        <div className="main-nav__container">
-          <button className="main-nav__btn">
+        <div className="main-nav__left">
+          <button className="main-nav__btn button">
             <MenuIcon />
           </button>
         </div>
-        <div className="main-nav__container">
+        <div className="main-nav__middle">
           <StockFetcher />
         </div>
-        <div className="main-nav__container">
+        <div className="main-nav__right">
           <button className="main-nav__login">login</button>
           <button className="main-nav__register">register</button>
         </div>
