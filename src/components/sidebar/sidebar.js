69fcb7b76d58d8658fe3f4741f667a0570b2b7ae
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-08 23:38:34 -0700

    rewrote html & css

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,39 +1,24 @@
 import React from 'react';
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { toggleSidebar } from '../../redux/';
 import { Routes } from '../routes/';
 import { LrnzIcon } from '../../icons/';
 import { ArrowIcon } from '../../icons/';
+import { useToggler } from '../../hooks/';
 import './style.scss';
 
 const Sidebar = () => {
-  const [toggled, setToggled] = useState(true);
-  const { className } = useSelector(state => state.uiReducer);
-  const dispatch = useDispatch();
-
-  const onClick = () => {
-    setToggled(!toggled);
-    dispatch(toggleSidebar(toggled));
-  };
+  const [className, toggle] = useToggler();
 
   return (
     <section className={`sidebar ${className}`}>
-      <div className="sidebar-logo-container">
-        <div className="sidebar-logo">
-          <LrnzIcon />
-        </div>
-        <div className="sidebar-brand-container">
-          <h2 className={`sidebar-brand-name ${className}`}>LRNZ</h2>
-        </div>
+      <div className="sidebar-header">
+        <LrnzIcon />
+        <p className={`sidebar-header__brand ${className}`}>LRNZ</p>
       </div>
-      <div className="sidebar-contents">
+      <div className="sidebar-routes">
         <Routes />
       </div>
-      {/* eslint-disable */}
-      <div className="sidebar-footer" onClick={onClick}>
-        <button className="button">
+      <div className="sidebar-footer">
+        <button className="sidebar-footer__btn" onClick={toggle}>
           <ArrowIcon />
         </button>
       </div>
