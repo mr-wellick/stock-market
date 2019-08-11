f642eab9b778c62b759dddc23e9ee8af424ff846
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-10 20:55:40 -0700

    completed news

--- src/components/news/news.js
+++ src/components/news/news.js
@@ -8,28 +8,24 @@ const News = () => {
   const news = data[activeStock].news;
   console.log(news);
 
-  return (
-    <div>
-      {news.map(article => (
-        <div key={article.url}>
-          <div>
-            <h3>
-              <b>{article.headline}</b>
-            </h3>
-          </div>
-          <div>
-            <p>{article.summary.substring(0, 250)}...</p>
-          </div>
-          <div>
-            <a href={article.url} target="_blank" rel="noopener noreferrer">
-              {article.source}
-            </a>
-            <span>{new Date(article.datetime).toLocaleString()}</span>
-          </div>
-        </div>
-      ))}
+  return news.map(article => (
+    <div className="news-container" key={article.url}>
+      <div className="news-headline">
+        <h3>
+          <b>{article.headline}</b>
+        </h3>
+      </div>
+      <div className="news-summary">
+        <p>{article.summary}</p>
+      </div>
+      <div className="news-source">
+        <a href={article.url} target="_blank" rel="noopener noreferrer">
+          {article.source}
+        </a>
+        <span>{new Date(article.datetime).toLocaleString()}</span>
+      </div>
     </div>
-  );
+  ));
 };
 
 export default News;
