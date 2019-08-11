cb1a6406c546d4f8d4e03a4b548834fee4566241
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-10 20:10:15 -0700

    check before accessing data

--- src/hooks/useHistoricalData.js
+++ src/hooks/useHistoricalData.js
@@ -5,14 +5,18 @@ const useHistoricalData = () => {
 
   if (Object.keys(data).length === 0 || !data[activeStock]) return null;
 
-  const rawData = Object.entries(data[activeStock])[1][1];
+  if (data[activeStock]['Monthly Adjusted Time Series']) {
+    const rawData = Object.entries(data[activeStock])[1][1];
 
-  const formatted = Object.entries(rawData).map(items => ({
-    date: new Date(items[0]),
-    close: Number(items[1]['5. adjusted close'])
-  }));
+    const formatted = Object.entries(rawData).map(items => ({
+      date: new Date(items[0]),
+      close: Number(items[1]['5. adjusted close'])
+    }));
 
-  return formatted;
+    return formatted;
+  }
+
+  return null;
 };
 
 export default useHistoricalData;
