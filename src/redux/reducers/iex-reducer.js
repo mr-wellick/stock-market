7f4e6072e30292b33e2acf01a6707a958d9c7beb
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-10 18:07:11 -0700

    fixed error when data isn't available

--- src/redux/reducers/iex-reducer.js
+++ src/redux/reducers/iex-reducer.js
@@ -7,7 +7,6 @@ function iexReducer(state = { data: { TSLA }, error: '', activeStock: 'TSLA' }, 
   if (action.type === FETCH_IEX_DATA) {
     return {
       ...state,
-      activeStock: Object.keys(action.payload)[0],
       data: {
         ...state.data,
         ...action.payload
