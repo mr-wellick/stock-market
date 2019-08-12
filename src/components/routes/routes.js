19c0f056978f8b8b91588ab148c9ec91d7b1dbc5
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-12 01:22:00 -0700

    refactored html & css

--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -1,41 +1,38 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import { DashboardIcon } from '../../icons/';
-//import { FinancialsIcon } from '../../icons/';
+import { FinancialsIcon } from '../../icons/';
 import './style.scss';
+
+// Note: Form of type radio doesn't play nicely with imbedded <Links/> within the
+//       <label/> tag.
 
 const Routes = () => {
   const { className } = useSelector(state => state.uiReducer);
 
   return (
-    <form className="form-routes">
+    <form className="routes">
       <div className="route-container">
         <input
           type="radio"
-          id="dashboard"
           name="route-selector"
+          id="dashboard"
           className="route-input"
           defaultChecked
         />
-        <Link to="/" className="route-link">
+        <label htmlFor="dashboard" className="route-label">
           <DashboardIcon />
-          <label htmlFor="dashboard" className={`route-label ${className}`}>
-            Dashboard
-          </label>
-        </Link>
+          <span className={`route-name ${className}`}>Dashboard</span>
+        </label>
       </div>
-      {/*
       <div className="route-container">
-        <input type="radio" id="financials" name="route-selector" className="route-input" />
-        <Link to="/financials/" className="route-link">
+        <input type="radio" name="route-selector" id="financials" className="route-input" />
+        <label htmlFor="financials" className="route-label">
           <FinancialsIcon />
-          <label htmlFor="financials" className={`route-label ${className}`}>
-            Financials
-          </label>
-        </Link>
+          <span className={`route-name ${className}`}>Financials</span>
+        </label>
       </div>
-      */}
     </form>
   );
 };
