501c3f6b7de45f810653b92122776bd9f79526cf
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-12 01:39:17 -0700

    added search icon

--- src/icons/index.js
+++ src/icons/index.js
@@ -3,3 +3,4 @@ export { default as FinancialsIcon } from './financials-icon.js';
 export { default as LrnzIcon } from './lrnz-icon.js';
 export { default as MenuIcon } from './menu-icon.js';
 export { default as ArrowIcon } from './arrow-icon.js';
+export { default as SearchIcon } from './search-icon.js';
