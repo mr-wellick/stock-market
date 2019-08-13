c993730bde42123fd5c5d4ce014bbed7481b2b0d
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-13 05:52:03 -0700

    add more data

--- src/mock-data/financials.js
+++ src/mock-data/financials.js
@@ -1,6 +1,8 @@
 export const financials = {
   grid: [
     [{ value: '' }, { value: 'Q1.18' }, { value: 'Q2.18' }, { value: 'Q3.18' }],
+    [{ value: 'Revenues' }, { value: '' }, { value: '' }, { value: '' }],
+    [{ value: '' }, { value: '' }, { value: '' }, { value: '' }],
     [{ value: 'Auto Sales' }, { value: 2561881000 }, { value: 3117865000 }, { value: 5878305000 }],
     [{ value: 'Auto Leasing' }, { value: 173436000 }, { value: 239816000 }, { value: 220461000 }],
     [
@@ -15,6 +17,37 @@ export const financials = {
       { value: 270142000 },
       { value: 326330000 }
     ],
-    [{ value: 'Revenue' }, { value: 3408751000 }, { value: 4002231000 }, { value: 6824413000 }]
+    [
+      { value: 'Total Revenue' },
+      { value: 3408751000 },
+      { value: 4002231000 },
+      { value: 6824413000 }
+    ],
+    [{ value: '' }, { value: '' }, { value: '' }, { value: '' }],
+    [{ value: 'Cost of Revenues' }, { value: '' }, { value: '' }, { value: '' }],
+    [{ value: '' }, { value: '' }, { value: '' }, { value: '' }],
+    [{ value: 'Auto Sales' }, { value: 2561881000 }, { value: 3117865000 }, { value: 5878305000 }],
+    [{ value: 'Auto Sales' }, { value: 2091397000 }, { value: 2529739000 }, { value: 4405919000 }],
+    [{ value: 'Auto Leasing' }, { value: 104496000 }, { value: 136915000 }, { value: 119283000 }],
+    [
+      { value: 'Energy Gen & Storage' },
+      { value: 375363000 },
+      { value: 330273000 },
+      { value: 330554000 }
+    ],
+    [
+      { value: 'Sevices & Others' },
+      { value: 380969000 },
+      { value: 386374000 },
+      { value: 444992000 }
+    ],
+    [
+      { value: 'Total Cost of Revenue' },
+      { value: 2952225000 },
+      { value: 3383301000 },
+      { value: 5300748000 }
+    ],
+    [{ value: '' }, { value: '' }, { value: '' }, { value: '' }],
+    [{ value: 'Gross Profit' }, { value: 456526000 }, { value: 618930000 }, { value: 1523665000 }]
   ]
 };
