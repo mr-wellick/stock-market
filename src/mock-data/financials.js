b8f94112d41fa9839515019f65513469ebb94eff
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-13 05:15:37 -0700

    broke out mock data to its own file

---
+++ src/mock-data/financials.js
@@ -0,0 +1,20 @@
+export const financials = {
+  grid: [
+    [{ value: '' }, { value: 'Q1.18' }, { value: 'Q2.18' }, { value: 'Q3.18' }],
+    [{ value: 'Auto Sales' }, { value: 2561881000 }, { value: 3117865000 }, { value: 5878305000 }],
+    [{ value: 'Auto Leasing' }, { value: 173436000 }, { value: 239816000 }, { value: 220461000 }],
+    [
+      { value: 'Energy Gen & Storage' },
+      { value: 410022000 },
+      { value: 374408000 },
+      { value: 399317000 }
+    ],
+    [
+      { value: 'Sevices & Others' },
+      { value: 263412000 },
+      { value: 270142000 },
+      { value: 326330000 }
+    ],
+    [{ value: 'Revenue' }, { value: 3408751000 }, { value: 4002231000 }, { value: 6824413000 }]
+  ]
+};
