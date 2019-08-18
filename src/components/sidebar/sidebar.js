b9a62d51e22a3c224d8e6d1bf719ddcc75b9b0f7
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-18 07:16:53 -0700

    mobile first approach

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -5,10 +5,10 @@ import './style.scss';
 
 const Sidebar = () => {
   return (
-    <section className={'sidebar'}>
+    <section className="sidebar">
       <div className="sidebar-header">
         <LrnzIcon />
-        <p className={'sidebar-header__brand'}>LRNZ</p>
+        <p className="sidebar-header__brand">LRNZ</p>
       </div>
       <div className="sidebar-routes">
         <Routes />
