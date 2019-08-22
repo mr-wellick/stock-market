4bb49a528eb58cd0015df6914f66450e7d65c52b
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-22 15:18:20 -0700

    removed news component, which used mock data

--- src/views/dashboard/dashboard.js
+++ src/views/dashboard/dashboard.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { NavBar } from '../../components/';
 import { StockSelector } from '../../components/';
 import { HistoricalChart } from '../../components/';
-import { News } from '../../components/';
 import './style.scss';
 
 const Dashboard = () => {
@@ -14,10 +13,6 @@ const Dashboard = () => {
       <div className="main-content__data">
         <StockSelector />
         <HistoricalChart />
-      </div>
-      <div className="main-content__news">
-        <h1>News</h1>
-        <News />
       </div>
     </>
   );
