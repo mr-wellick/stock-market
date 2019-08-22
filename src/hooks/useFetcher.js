df14a25bbfbe7b8ae28ecc45d36e0319a83f7065
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-22 15:16:44 -0700

    now making api call

--- src/hooks/useFetcher.js
+++ src/hooks/useFetcher.js
@@ -1,19 +1,18 @@
-//import { useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { validate } from '../utilities/';
 
 const useFetcher = node => {
   const { data } = useSelector(state => state.iexReducer);
   const { input } = useSelector(state => state.uiReducer);
-  //const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const fetchStock = event => {
     event.preventDefault();
     const validStockName = validate(input);
 
     if (validStockName && !data[validStockName]) {
-      //dispatch({ type: 'FETCH_REQUESTED', validStockName });
-      console.log(`valid stock ticker: ${validStockName}`);
+      dispatch({ type: 'FETCH_REQUESTED', validStockName });
     } else {
       console.log('woooops');
     }
