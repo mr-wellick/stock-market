99ed43eafa2d4a7fae54e52fb83a45190546f1b8
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-22 15:21:00 -0700

    removed news component

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,3 @@ export { NavBar } from './nav-bar/';
 export { Sidebar } from './sidebar/';
 export { StockSelector } from './stock-selector/';
 export { HistoricalChart } from './historical-chart/';
-export { News } from './news/';
