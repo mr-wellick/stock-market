32e216249c526b03386c1c24c5b9b611d84221e3
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-22 15:20:38 -0700

    rewrote logic to handle new data structure

--- src/hooks/useHistoricalData.js
+++ src/hooks/useHistoricalData.js
@@ -3,15 +3,20 @@ import { useSelector } from 'react-redux';
 const useHistoricalData = () => {
   const { data, activeStock } = useSelector(state => state.iexReducer);
 
-  if (Object.keys(data).length === 0 || !data[activeStock]) return null;
+  if (data[activeStock] && data[activeStock]['Monthly Adjusted Time Series']) {
+    const prices = data[activeStock]['Monthly Adjusted Time Series'];
+    const formattedData = [];
 
-  if (data[activeStock].chart) {
-    const formatted = data[activeStock].chart.map(historical => ({
-      ...historical,
-      date: new Date(historical['date'])
-    }));
+    // don't know why eslint is complaining
+    /* eslint-disable */
+    for (const month in prices) {
+      formattedData.push({
+        date: new Date(month),
+        close: Number(prices[month]['5. adjusted close'])
+      });
+    }
 
-    return formatted;
+    return formattedData;
   }
 
   return null;
