adab0e675bfdfa5225f31a082b2b0dfdd3382943
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-22 15:21:07 -0700

    Rewrote company name banner [skip ci]

--- src/components/company-name/company-name.js
+++ src/components/company-name/company-name.js
@@ -5,22 +5,14 @@ import './style.scss';
 const CompanyName = () => {
   const { data, activeStock } = useSelector(state => state.iexReducer);
 
-  if (Object.keys(data).length === 0 || !data[activeStock]) return null;
-
-  const { companyName, symbol, close, change, changePercent } = data[activeStock].quote;
-  const trend = change < 0 ? 'trend-down' : 'trend-up';
+  if (!data[activeStock] && !data[activeStock]['Meta Data']) {
+    return;
+  }
 
   return (
     <div className="company-info-container">
-      <p className="company-name-container">
-        <span className="company-name">{companyName}</span>
-        <span className="company-symbol">[{symbol}]</span>
-      </p>
-      <p>
-        <span className="company-close">{close}</span>
-        <span className={trend}>{change}</span>
-        <span className={trend}>({changePercent})</span>
-      </p>
+      <p className="company-name">[{data[activeStock]['Meta Data']['2. Symbol']}]</p>
+      <p>{data[activeStock]['Meta Data']['3. Last Refreshed']}</p>
     </div>
   );
 };
