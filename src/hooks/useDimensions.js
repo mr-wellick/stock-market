ad260b3f8cdfed4fb3b207ab41dd09c2b5f4d5fc
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-23 09:13:26 -0700

    made chart responsive

--- src/hooks/useDimensions.js
+++ src/hooks/useDimensions.js
@@ -1,10 +1,27 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 const useDimensions = () => {
   const [dimensions, setDimensions] = useState({
     width: window.innerWidth,
     height: window.innerHeight * 0.55,
     padding: 35
+  });
+
+  const resize = () => {
+    setDimensions({
+      ...dimensions,
+      width: window.innerWidth,
+      height: window.innerHeight * 0.55
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', resize);
+
+    return function cleanUp() {
+      window.removeEventListener('resize', resize);
+    };
   });
 
   return [dimensions, setDimensions];
