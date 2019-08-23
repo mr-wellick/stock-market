42ea4adc378ea8c7b1aeb27bd0f2a5cf0d465ca5
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-23 10:20:18 -0700

    removed useSelector

--- src/components/routes/routes.js
+++ src/components/routes/routes.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 //import { Link } from 'react-router-dom';
 import { DashboardIcon } from '../../icons/';
 import { FinancialsIcon } from '../../icons/';
@@ -9,8 +8,6 @@ import './style.scss';
 //       <label/> tag.
 
 const Routes = () => {
-  const { className } = useSelector(state => state.uiReducer);
-
   return (
     <form className="routes">
       <div className="route-container">
@@ -23,14 +20,14 @@ const Routes = () => {
         />
         <label htmlFor="dashboard" className="route-label">
           <DashboardIcon />
-          <span className={`route-name ${className}`}>Dashboard</span>
+          <span className="route-name">Dashboard</span>
         </label>
       </div>
       <div className="route-container">
         <input type="radio" name="route-selector" id="financials" className="route-input" />
         <label htmlFor="financials" className="route-label">
           <FinancialsIcon />
-          <span className={`route-name ${className}`}>Financials</span>
+          <span className="route-name">Financials</span>
         </label>
       </div>
     </form>
