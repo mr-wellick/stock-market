1569a7bc6d0fa3106c7366befb73566826a6f320
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-23 09:44:06 -0700

    refactored to work with responsive-nav as well

--- src/hooks/useToggler.js
+++ src/hooks/useToggler.js
@@ -1,17 +1,20 @@
-import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { toggleSidebar } from '../redux/';
 
 const useToggler = () => {
-  const [toggled, setToggled] = useState(true);
   const dispatch = useDispatch();
+  const { className: toggle } = useSelector(state => state.uiReducer);
 
-  const toggle = () => {
-    setToggled(!toggled);
-    dispatch(toggleSidebar(toggled));
+  const setToggle = () => {
+    if (toggle === '') {
+      dispatch(toggleSidebar(true));
+    } else {
+      dispatch(toggleSidebar(false));
+    }
   };
 
-  return toggle;
+  return [toggle, setToggle];
 };
 
 export default useToggler;
