3069727b2d010cdd00568a31f96696b8ceb206cd
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-23 10:19:44 -0700

    removed sidebar

--- src/app.js
+++ src/app.js
@@ -3,14 +3,12 @@ import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route } from 'react-router-dom';
-import { Sidebar } from './components/';
 import { Footer } from './components/';
 import { Dashboard } from './views/';
 
 const App = () => {
   return (
     <Router>
-      <Sidebar />
       <section className="main-content">
         <Route exact path="/" component={Dashboard} />
         <Route path="/" component={Footer} />
