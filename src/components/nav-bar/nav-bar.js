31663cced65fd145ef81d9925ac81872fc08edc5
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-27 15:27:31 -0700

    rewrote css and html

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -1,38 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { StockFetcher } from '../stock-fetcher/';
-import { Dropdown } from '../dropdown/';
-import { useToggler } from '../../hooks/';
 import { MenuIcon } from '../../icons/';
-import { LrnzIcon } from '../../icons/';
 import './style.scss';
 
 const NavBar = () => {
-  const [, toggle] = useToggler();
-
   return (
-    <header className="main-header">
-      <nav className="main-nav">
-        <div className="main-nav__left">
-          <div className="main-nav__left-icon">
-            <LrnzIcon />
-          </div>
-          <StockFetcher />
-          <Dropdown />
-        </div>
-        <div className="main-nav__right">
-          <button className="main-nav__btn">
-            <Link to="/login">login</Link>
-          </button>
-          <button className="main-nav__btn">
-            <Link to="/register">register</Link>
-          </button>
-          {/*eslint-disable*/}
-          <div className="main-nav__menu" onClick={toggle}>
-            <MenuIcon />
-          </div>
-        </div>
-      </nav>
+    <header className="nav-container">
+      <StockFetcher />
+      <div className="nav-buttons">
+        <Link className="nav-login" to="/login">
+          login
+        </Link>
+        <Link className="nav-register" to="/register">
+          register
+        </Link>
+      </div>
     </header>
   );
 };
