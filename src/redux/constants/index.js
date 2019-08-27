0de15dca072f9ea402607a6c786ff4b62139ac30
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-27 09:43:14 -0700

    added new redux actions

--- src/redux/constants/index.js
+++ src/redux/constants/index.js
@@ -5,3 +5,17 @@ export const SET_ACTIVE_STOCK = 'SET_ACTIVE_STOCK';
 export const USER_INPUT = 'USER_INPUT';
 export const FETCH_REQUESTED = 'FETCH_REQUESTED';
 export const BEST_MATCHES = 'BEST_MATCHES';
+
+// user input
+export const UPDATE_STOCK_TICKER_INPUT = 'UPDATE_STOCK_TICKER_INPUT';
+
+// available stock tickers
+export const IS_FETCHING_STOCK_SYMBOLS = 'IS_FETCHING_STOCK_SYMBOLS';
+export const FETCH_STOCK_SYMBOLS = 'FETCH_STOCK_SYMBOLS';
+
+// retrieve stock data for single stock ticker
+export const IS_FETCHING_STOCK_TICKER_DATA = 'IS_FETCHING_STOCK_TICKER_DATA';
+export const FETCH_STOCK_TICKER_DATA = 'FETCH_STOCK_TICKER_DATA';
+
+// responsive navigation
+export const TOGGLE_RESPONSIVE_NAV = 'TOGGLE_RESPONSIVE_NAV';
