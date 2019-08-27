d2ad59d991149c25fa7fd7b558e2b61ac8b9b0c5
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-27 13:21:31 -0700

    removed composeWithDevTools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import { createStore } from 'redux';
 import { applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers/';
 import rootSaga from './sagas/';
@@ -8,8 +7,10 @@ import rootSaga from './sagas/';
 const sagaMiddleware = createSagaMiddleware();
 let store;
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
 if (process.env.NODE_ENV === 'development') {
-  store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+  store = createStore(rootReducer, composeEnhancer(applyMiddleware(sagaMiddleware)));
 } else {
   store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 }
