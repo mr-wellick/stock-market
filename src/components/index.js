a4904cbcac3f8f1fa12ac3458e183acd5b794b09
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-27 17:21:55 -0700

    removed unused exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,2 @@
 export { NavBar } from './nav-bar/';
 export { Sidebar } from './sidebar/';
-export { StockSelector } from './stock-selector/';
-export { HistoricalChart } from './historical-chart/';
-export { Footer } from './footer/';
