7e960ff9da01725153655719301d795943eac986
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-27 17:17:18 -0700

    base responsive layout complete

--- src/components/stock-fetcher/stock-fetcher.js
+++ src/components/stock-fetcher/stock-fetcher.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import { SearchIcon } from '../../icons/';
-import { userInput } from '../../redux/';
 import './style.scss';
 
 const StockFetcher = () => {
-  const dispatch = useDispatch();
-
   return (
     <form className="stock-form" autoComplete="off">
       <SearchIcon />
-      <input
-        onChange={event => dispatch(userInput(event.target.value))}
-        className="stock-input"
-        type="text"
-        placeholder="Search Symbols"
-        required
-      />
+      <input className="stock-input" type="text" placeholder="Search Symbols" required />
     </form>
   );
 };
