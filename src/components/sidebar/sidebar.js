0f07cab1a43a61846f9d30e482067a8be90c4d1c
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-27 17:16:21 -0700

    base responsive sidebar complete

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Routes } from '../routes/';
 import { LrnzIcon } from '../../icons/';
+import { ExitIcon } from '../../icons/';
+import { useToggler } from '../../hooks/';
 import './style.scss';
 
 const Sidebar = () => {
+  const [toggled, setToggled] = useToggler();
+
   return (
-    <section className="sidebar">
+    <section className={`sidebar ${toggled}`}>
       <div className="sidebar-header">
         <LrnzIcon />
         <p className="sidebar-brand">LRNZ</p>
+        {/* eslint-disable */}
+        <div className="sidebar-btn" onClick={setToggled}>
+          <ExitIcon />
+        </div>
       </div>
       <div className="sidebar-routes">
         <Routes />
