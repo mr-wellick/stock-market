b7ba5f8d6e01fd6f9ceccc9ca37d97026b91744f
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-28 10:27:54 -0700

    intergrated epic into redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,20 @@
+import { createEpicMiddleware } from 'redux-observable';
 import { createStore } from 'redux';
+import { applyMiddleware } from 'redux';
 import rootReducer from './reducers/';
+import { rootEpic } from './epics/';
+
+const epicMiddleware = createEpicMiddleware();
 
 let store;
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
 if (process.env.NODE_ENV === 'development') {
-  store = createStore(rootReducer, composeEnhancer());
+  store = createStore(rootReducer, composeEnhancer(applyMiddleware(epicMiddleware)));
 } else {
-  store = createStore(rootReducer);
+  store = createStore(rootReducer, applyMiddleware(epicMiddleware));
 }
+
+epicMiddleware.run(rootEpic);
 
 export default store;
