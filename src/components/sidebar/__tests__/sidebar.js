ca77d8bd53e60dd6d2007541e08b2fa4aa41ecfc
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-27 20:09:19 -0700

    added placeholder test

---
+++ src/components/sidebar/__tests__/sidebar.js
@@ -0,0 +1,7 @@
+//import React from 'react';
+//import { render } from '@testing-library/react';
+//import { fireEvent } from '@testing-library/react';
+
+test('adds className is-open when button is clicked', () => {
+  console.log('test');
+});
