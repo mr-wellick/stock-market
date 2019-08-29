d5177710a34dfda618ea816a0c4f5b839837ddf0
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-29 16:44:55 -0700

    Changed paramerter name and added new action
      - Change stockName to input
      - Added new action SET_ACTIVE_STOCK

--- src/redux/reducers/stock-data-reducer.js
+++ src/redux/reducers/stock-data-reducer.js
@@ -1,22 +1,30 @@
 import { FETCH_STOCK_DATA } from '../constants/';
 import { FETCH_STOCK_DATA_SUCCESS } from '../constants/';
+import { SET_ACTIVE_STOCK } from '../constants/';
 
-function stockDataReducer(state = { stockName: '', stockData: {}, activeStock: '' }, action) {
+function stockDataReducer(state = { input: '', stockData: {}, activeStock: '' }, action) {
   if (action.type === FETCH_STOCK_DATA) {
     return {
       ...state,
-      stockName: action.payload.stockName
+      input: action.payload.input
     };
   }
 
   if (action.type === FETCH_STOCK_DATA_SUCCESS) {
     return {
       ...state,
-      activeStock: state.stockName,
+      activeStock: state.input,
       stockData: {
         ...state.stockData,
-        [state.stockName]: action.payload.stockData
+        [state.input]: action.payload.stockData
       }
+    };
+  }
+
+  if (action.type === SET_ACTIVE_STOCK) {
+    return {
+      ...state,
+      activeStock: action.payload.activeStock
     };
   }
 
