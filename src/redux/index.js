8378b344a8016fe7f229efc4df2322b009630390
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-29 16:46:45 -0700

    changed activeStock to setActiveStock

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,6 +2,7 @@
 export { default as store } from './store.js';
 
 // action creators
+export { setActiveStock } from './actions/';
 export { sidebarToggled } from './actions';
 export { queryStockTickers } from './actions/';
 export { stockTickersSuccess } from './actions/';
