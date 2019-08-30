f5146774988f6a2562d308d094fd4fe4badd1e6e
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-30 14:36:16 -0700

    finished loader component

--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -4,7 +4,7 @@ import './style.scss';
 const Loader = () => {
   return (
     <div className="loader-container">
-      <div>
+      <div className="loader-icon">
         <div id="block1"></div>
         <div id="block2"></div>
         <div id="block3"></div>
