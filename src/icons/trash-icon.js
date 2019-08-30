dc290081ac2e9de0019b97233c5017146a1b60d4
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-29 22:10:11 -0700

    added data-symbol custom attribute

--- src/icons/trash-icon.js
+++ src/icons/trash-icon.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const TrashIcon = () => {
+const TrashIcon = props => {
   return (
-    <svg height="18px" width="18px" viewBox="0 0 18 18" fill="#3367d6">
-      <path d="M6.5 3H3v2h12V3h-3.5c0-.556-.448-1-1-1h-3c-.556 0-1 .448-1 1zM4 6h10v8c0 1.105-.887 2-2 2H6c-1.105 0-2-.887-2-2V6z" />
+    <svg height="18px" width="18px" viewBox="0 0 18 18" fill="#3367d6" data-symbol={props.symbol}>
+      <path
+        data-symbol={props.symbol}
+        d="M6.5 3H3v2h12V3h-3.5c0-.556-.448-1-1-1h-3c-.556 0-1 .448-1 1zM4 6h10v8c0 1.105-.887 2-2 2H6c-1.105 0-2-.887-2-2V6z"
+      />
     </svg>
   );
 };
