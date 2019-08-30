fd49c230b771ffcc1b60df8475439ad2fac8cfdc
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-29 22:06:31 -0700

    removed comments

--- src/components/historical-chart/historical-chart.js
+++ src/components/historical-chart/historical-chart.js
@@ -1,25 +1,25 @@
 import React from 'react';
-//import { GGPLOT } from 'react-d3-ggplot';
-//import { Line } from 'react-d3-ggplot';
-//import { useDimensions } from '../../hooks/';
+import { GGPLOT } from 'react-d3-ggplot';
+import { Line } from 'react-d3-ggplot';
+import { useDimensions } from '../../hooks/';
 import { useHistoricalData } from '../../hooks/';
 import { CompanyName } from '../company-name/';
 import './style.scss';
 
 const HistoricalChart = () => {
   const historicalData = useHistoricalData();
-  //const [dimensions] = useDimensions();
+  const [dimensions] = useDimensions();
 
   if (!historicalData) return null;
 
   return (
     <div className="historical-chart">
       <CompanyName />
-      {/*
       <GGPLOT data={historicalData} aes={['date', 'close']} dimensions={dimensions}>
         <Line />
       </GGPLOT>
-      */}
+      {/*
+       */}
     </div>
   );
 };
