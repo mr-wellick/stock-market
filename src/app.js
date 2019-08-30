add6d61eba862a025dde5d88401004bf57f470d7
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-30 15:00:29 -0700

    added loader to show on initial page load

--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
 import './app.scss';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { hot } from 'react-hot-loader/root';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { Footer } from './components/';
 import { Sidebar } from './components/';
 import { Modal } from './components/';
+import { Loader } from './components/';
 import { Dashboard } from './views/';
 
 const App = () => {
+  const { stockData, isLoading } = useSelector(state => state.stockDataReducer);
+
   return (
     <>
       <Modal />
       <Router>
         <Sidebar />
         <section className="content-container">
+          {Object.keys(stockData).length === 0 && isLoading ? <Loader /> : null}
           <Route exact path="/" component={Dashboard} />
           <Route path="/" component={Footer} />
         </section>
