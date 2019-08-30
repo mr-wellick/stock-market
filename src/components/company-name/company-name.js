478165c9ef9a20d3a4f171f1756119862dc05a88
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-29 17:14:29 -0700

    changed stockName to activeStock

--- src/components/company-name/company-name.js
+++ src/components/company-name/company-name.js
@@ -3,16 +3,16 @@ import { useSelector } from 'react-redux';
 import './style.scss';
 
 const CompanyName = () => {
-  const { stockName, stockData } = useSelector(state => state.stockDataReducer);
+  const { stockData, activeStock } = useSelector(state => state.stockDataReducer);
 
-  if (!stockData[stockName]['Meta Data']) {
+  if (!stockData[activeStock]['Meta Data']) {
     return;
   }
 
   return (
     <div className="company-info-container">
-      <p className="company-name">[{stockData[stockName]['Meta Data']['2. Symbol']}]</p>
-      <p>{stockData[stockName]['Meta Data']['3. Last Refreshed']}</p>
+      <p className="company-name">[{stockData[activeStock]['Meta Data']['2. Symbol']}]</p>
+      <p>{stockData[activeStock]['Meta Data']['3. Last Refreshed']}</p>
     </div>
   );
 };
