ec20e2f2370f6d5674783ea006de0c36f720a84f
Pablo Lorenzo <lorenz3192@gmail.com>
2019-08-30 12:49:32 -0700

    finished modal component

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { useRef } from 'react';
 import { createPortal } from 'react-dom';
+import { toggleModal } from '../../redux/';
 import './style.scss';
 
-const Modal = ({ children }) => {
+const ModalHelper = ({ children }) => {
   const node = useRef(null);
 
   if (!node.current) {
@@ -21,7 +24,30 @@ const Modal = ({ children }) => {
     };
   }, []);
 
-  return createPortal(<div>{children}</div>, node.current);
+  return createPortal(<>{children}</>, node.current);
+};
+
+const Modal = () => {
+  const dispatch = useDispatch();
+  const { message } = useSelector(state => state.uiReducer);
+
+  return message.error ? (
+    <ModalHelper>
+      <div className="modal-container">
+        <div className="modal-card">
+          <div className="modal-content">
+            <h4 className="modal-header">Message</h4>
+            <p className="modal-message">{message.error}</p>
+          </div>
+          <div className="modal-footer">
+            <button className="modal-btn" onClick={() => dispatch(toggleModal({}))}>
+              close
+            </button>
+          </div>
+        </div>
+      </div>
+    </ModalHelper>
+  ) : null;
 };
 
 export default Modal;
