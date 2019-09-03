dec9f8f94739f1bc768b43286f82a3b84ee9cb83
Pablo Lorenzo <lorenz3192@gmail.com>
2019-09-03 16:47:51 -0700

    login view complete

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { LrnzIcon } from '../../icons/';
 import './style.scss';
 
 const Login = () => {
   return (
     <div className="login-container">
-      <Link to="/">Home</Link>
-      <form>
-        <input type="text" id="login" />
-      </form>
+      <div className="login-card">
+        <div className="login-header">
+          <Link to="/">
+            <LrnzIcon />
+          </Link>
+        </div>
+        <div className="login-content">
+          <form>
+            <input type="text" placeholder="Email" />
+            <input type="text" placeholder="Password" />
+          </form>
+        </div>
+        <div className="login-btn">
+          <button>Login</button>
+        </div>
+        <div className="login-footer">
+          <Link to="/register">Create an account</Link>
+        </div>
+      </div>
+      <div className="lrnz-message">
+        <p>Developed by LRNZ</p>
+      </div>
     </div>
   );
 };
