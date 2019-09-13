7abc912847fd6f5ae9d9b22bf55d3664149a1f32
Pablo Lorenzo <lorenz3192@gmail.com>
2019-09-13 12:58:11 -0700

    added download icon

---
+++ src/icons/download-icon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const DownloadIcon = () => {
+  return (
+    <svg height="14px" width="14px" fill="#222" viewBox="0 0 60 60">
+      <g>
+        <polygon points="54 24.75 36 24.75 36 0 24 0 24 24.75 6 24.75 30 48.75 54 24.75"></polygon>
+        <rect y="54" width="60" height="6"></rect>
+      </g>
+    </svg>
+  );
+};
+
+export default DownloadIcon;
