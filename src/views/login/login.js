5addcf50e27ffde8b479d115a151f74f206487b3
Pablo Lorenzo <lorenz3192@gmail.com>
2020-01-07 18:42:51 -0800

    reset code

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,35 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { LrnzIcon } from '../../icons/';
+//import { Link } from 'react-router-dom';
+//import { LrnzIcon } from '../../icons/';
 import './style.scss';
 
 const Login = () => {
-  return (
-    <div className="login-container">
-      <div className="login-card">
-        <div className="login-header">
-          <Link to="/">
-            <LrnzIcon />
-          </Link>
-        </div>
-        <div className="login-content">
-          <form>
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Password" />
-          </form>
-        </div>
-        <div className="login-btn">
-          <button>Login</button>
-        </div>
-        <div className="login-footer">
-          <Link to="/register">Create an account</Link>
-        </div>
-      </div>
-      <div className="lrnz-message">
-        <p>Developed by LRNZ</p>
-      </div>
-    </div>
-  );
+  return <div>login</div>;
 };
 
 export default Login;
--- src/views/register/register.js
+++ src/views/register/register.js
@@ -1,37 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { LrnzIcon } from '../../icons/';
+//import { Link } from 'react-router-dom';
+//import { LrnzIcon } from '../../icons/';
 import './style.scss';
 
 const Register = () => {
-  return (
-    <div className="login-container">
-      <div className="login-card">
-        <div className="login-header">
-          <Link to="/">
-            <LrnzIcon />
-          </Link>
-        </div>
-        <div className="login-content" id="register">
-          <form>
-            <input type="text" placeholder="Username" />
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Password" />
-            <input type="text" placeholder="Verify Password" />
-          </form>
-        </div>
-        <div className="login-btn">
-          <button>Register</button>
-        </div>
-        <div className="login-footer">
-          <Link to="/login">Already have an account?</Link>
-        </div>
-      </div>
-      <div className="lrnz-message">
-        <p>Developed by LRNZ</p>
-      </div>
-    </div>
-  );
+  return <div>register</div>;
 };
 
 export default Register;
