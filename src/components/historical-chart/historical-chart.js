2235bfcf3e5ba8ca8b231996a6617d734ebb9d1b
Pablo Lorenzo <lorenz3192@gmail.com>
2020-01-11 11:09:05 -0800

    refactored for new api

--- src/components/historical-chart/historical-chart.js
+++ src/components/historical-chart/historical-chart.js
@@ -4,7 +4,6 @@ import { Line } from 'react-d3-ggplot';
 import { useSelector } from 'react-redux';
 import { CompanyName } from '../company-name/';
 import { Loader } from '../loader/';
-import { ChartPlaceholder } from '../chart-placeholder/';
 import { useDimensions } from '../../hooks/';
 import { useHistoricalData } from '../../hooks/';
 import './style.scss';
@@ -15,12 +14,6 @@ const HistoricalChart = () => {
   const [dimensions] = useDimensions();
 
   if (!historicalData) return null;
-  if (historicalData['Note'])
-    return (
-      <div className="historical-chart">
-        <ChartPlaceholder message={historicalData['Note']} />
-      </div>
-    );
 
   return (
     <div className="historical-chart">
