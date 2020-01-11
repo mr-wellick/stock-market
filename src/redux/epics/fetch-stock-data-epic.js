6ded9f074088dc0d1cbb12e10cbbbb3b7e089372
Pablo Lorenzo <lorenz3192@gmail.com>
2020-01-11 11:08:09 -0800

    migrated to iex api

--- src/redux/epics/fetch-stock-data-epic.js
+++ src/redux/epics/fetch-stock-data-epic.js
@@ -1,24 +1,23 @@
 import { ajax } from 'rxjs/ajax';
 import { map } from 'rxjs/operators';
+import { of } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { ofType } from 'redux-observable';
 import { FETCH_STOCK_DATA } from '../constants/';
 import { fetchStockDataSuccess } from '../actions/';
 
-//const iex = `https://cloud.iexapis.com/v1/stock/${
-//  action.payload.input
-//}/batch?types=quote,company,news,chart&range=1m&last=10&token=${process.env.IEX_KEY}`
-
 const fetchStockDataEpic = action$ => {
   return action$.pipe(
     ofType(FETCH_STOCK_DATA),
-    mergeMap(action =>
-      ajax
-        .getJSON(
-          `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=${action.payload.input}&apikey=${process.env.ALPHA_VANTAGE_KEY}`
-        )
-        .pipe(map(response => fetchStockDataSuccess(response)))
-    )
+    mergeMap(action => {
+      const iex = `https://cloud.iexapis.com/v1/stock/${action.payload.input}/batch?types=quote,company,news,chart&range=1m&last=10&token=${process.env.IEX_KEY}`;
+
+      return ajax.getJSON(iex).pipe(
+        map(response => fetchStockDataSuccess(response)),
+        catchError(error => of({ type: 'TEST', payload: error.xhr.response, error: true }))
+      );
+    })
   );
 };
 
