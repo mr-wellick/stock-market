72677e3787e4c46075c5d298a9e6a2f5f0e1eb94
Pablo Lorenzo <lorenz3192@gmail.com>
2020-01-11 11:09:23 -0800

    refactored for new component

--- src/components/company-name/company-name.js
+++ src/components/company-name/company-name.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { DownloadCSV } from '../download-csv/';
+//import { DownloadCSV } from '../download-csv/';
 import './style.scss';
 
 const CompanyName = () => {
   const { stockData, activeStock } = useSelector(state => state.stockDataReducer);
 
-  if (!stockData[activeStock]['Meta Data']) {
+  if (!stockData[activeStock]) {
     return null;
   }
 
+  const { companyName, symbol, change, changePercent, close } = stockData[activeStock].quote;
+  const trend = change < 0 ? 'is-down' : 'is-up';
+
   return (
     <div className="company-info-container">
-      <p className="company-name">[{stockData[activeStock]['Meta Data']['2. Symbol']}]</p>
-      <DownloadCSV />
+      <p className="company-name">
+        {companyName}[{symbol}]{' '}
+      </p>
+      <span className="company-close">{close}</span>
+      <span className="company-change" id={trend}>
+        {change}({changePercent})
+      </span>
     </div>
   );
 };
