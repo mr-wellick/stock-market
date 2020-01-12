bffaa7777cc0b15688dbe186c84492d659236f01
Pablo Lorenzo <lorenz3192@gmail.com>
2020-01-12 15:06:55 -0800

    removed comments

--- src/views/dashboard/dashboard.js
+++ src/views/dashboard/dashboard.js
@@ -1,30 +1,27 @@
 import React from 'react';
-//import { NavBar } from '../../components/';
-//import { StockSelector } from '../../components/';
-//import { HistoricalChart } from '../../components/';
+import { NavBar } from '../../components/';
+import { StockSelector } from '../../components/';
+import { HistoricalChart } from '../../components/';
 import { Sidebar } from '../../components/';
-//import { Footer } from '../../components/';
-//import { CompanyDescription } from '../../components/';
+import { CompanyDescription } from '../../components/';
 import './style.scss';
 
 const Dashboard = () => {
   return (
     <>
       <Sidebar />
-      {/*
-        <div className="content-container">
-          <div className="dashboard-nav">
-            <NavBar />
-          </div>
-          <div className="dashboard-content">
-            <StockSelector />
-            <HistoricalChart />
-          </div>
-          <div className="dashboard-content">
-            <CompanyDescription />
-          </div>
+      <div className="content-container">
+        <div className="dashboard-nav">
+          <NavBar />
         </div>
-      */}
+        <div className="dashboard-content">
+          <StockSelector />
+          <HistoricalChart />
+        </div>
+        <div className="dashboard-content">
+          <CompanyDescription />
+        </div>
+      </div>
     </>
   );
 };
