ab86036ca009f8ce519c29f8778f242f555f024a
Ll2NZ <lorenz3192@gmail.com>
2020-05-06 00:57:21 -0700

    Reset dashboard view [skip ci]

--- src/views/dashboard/dashboard.js
+++ src/views/dashboard/dashboard.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { Sidebar } from '../../components/';
 import { Navigation } from '../../components/';
-import { StockSypnosis } from '../../components/';
 import { HistoricalChart } from '../../components/';
-import { CompanyDescription } from '../../components/';
-import { StockSelector } from '../../components/';
-import { News } from '../../components/';
-//import { StockTable } from '../../components/';
 import './style.scss';
 
 const Dashboard = () => {
@@ -15,17 +10,7 @@ const Dashboard = () => {
       <Sidebar />
       <Navigation />
       <div className="dashboard-content">
-        <div className="dashboard-stock-list">
-          <StockSelector />
-        </div>
-        <div className="dashboard-chart-info">
-          <StockSypnosis />
-          <HistoricalChart />
-        </div>
-      </div>
-      <div className="dashboard-content info">
-        <CompanyDescription />
-        <News />
+        <HistoricalChart />
       </div>
     </>
   );
