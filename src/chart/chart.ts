f48b057d0a5c9d343688e4b78b2336a1d00562fd
mr-wellick <lorenz3192@gmail.com>
2024-03-07 21:50:12 -0800

    chart renderend [skip ci]

--- src/chart/chart.ts
+++ src/chart/chart.ts
@@ -1,5 +1,6 @@
-import { scaleTime } from 'd3-scale';
-import { axisBottom } from '../utils/attribute';
+import { scaleLinear, scaleTime } from 'd3-scale';
+import { axisBottom, axisLeft, position } from '../utils/attribute';
+import { line } from 'd3-shape';
 
 interface MetaData {
   '1. Information': string;
@@ -51,37 +52,42 @@ async function getSymbol(symbol: string = 'IBM'): Promise<ChartData[] | null> {
   return data.reverse();
 }
 
-export async function xAxis() {
-  const data = await getSymbol();
-  const dim = { width: 1400, height: 500, padding: 100, scaleBy: 2 };
+const data = await getSymbol();
+const dim = { width: 1400, height: 500, padding: 100, scaleBy: 2 };
 
+const xMin = Math.min(...data!.map((datum) => datum.x.getTime()));
+const xMax = Math.max(...data!.map((datum) => datum.x.getTime()));
+const xScale = scaleTime()
+  .domain([xMin, xMax])
+  .range([dim.padding, dim.width - dim.padding]);
+
+const yMax = Math.max(...data!.map((datum) => datum.y));
+const yScale = scaleLinear()
+  .domain([0, yMax])
+  .range([dim.height - dim.padding, dim.padding]);
+
+export function xAxis() {
   if (!data) {
     console.log(data);
     return;
   }
 
-  const min = Math.min(...data.map((datum) => datum.x.getTime()));
-  const max = Math.max(...data.map((datum) => datum.x.getTime()));
-  const scale = scaleTime()
-    .domain([min, max])
-    .range([dim.padding, dim.width - dim.padding]);
-
-  const group = document.createElement('g');
+  const group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
   group.setAttribute('transform', `translate(0, ${dim.height - dim.padding})`);
 
-  const path = document.createElement('path');
-  path.setAttribute('d', axisBottom(scale)!);
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  path.setAttribute('d', axisBottom(xScale)!);
   group.append(path);
 
-  const ticks = scale.ticks().map((datum) => {
-    const tickGroup = document.createElement('g');
-    tickGroup.setAttribute('transform', `translate(${scale(datum)}, 0)`);
+  const ticks = xScale.ticks().map((datum) => {
+    const tickGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    tickGroup.setAttribute('transform', `translate(${position(xScale)(datum)}, 0)`);
 
-    const line = document.createElement('line');
+    const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
     line.setAttribute('y2', '6');
     line.setAttribute('stroke', '#000');
 
-    const text = document.createElement('text');
+    const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
     text.setAttribute('dy', '0.71em');
     text.setAttribute('y', '10');
     text.setAttribute('transform', 'rotate(45)');
@@ -93,6 +99,57 @@ export async function xAxis() {
   });
 
   group.append(...ticks);
+
+  document.querySelector('#chart')?.append(group);
+}
+
+export function yAxis() {
+  if (!data) {
+    console.log(data);
+    return;
+  }
+
+  const group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+  group.setAttribute('transform', `translate(${dim.padding}, 0)`);
+
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  path.setAttribute('d', axisLeft(yScale)!);
+  group.append(path);
+
+  const ticks = yScale.ticks().map((datum) => {
+    const tickGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    tickGroup.setAttribute('transform', `translate(0, ${position(yScale)(datum)})`);
+
+    const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    line.setAttribute('x2', '-6');
+    line.setAttribute('stroke', '#000');
+
+    const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    text.setAttribute('x', '-60');
+    text.innerHTML = `${datum}`;
+
+    tickGroup.append(line, text);
+
+    return tickGroup;
+  });
+
+  group.append(...ticks);
+  document.querySelector('#chart')?.append(group);
+}
+
+export function timeSeriesLine() {
+  const shape = line<{ x: Date; y: number }>()
+    .x((d) => xScale(d.x))
+    .y((d) => yScale(d.y));
+
+  const group = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  path.setAttribute('d', shape(data!)!);
+  path.setAttribute('stroke', '#000');
+  path.setAttribute('fill', 'none');
+  path.setAttribute('strokeWidth', '2');
+
+  group.append(path);
 
   document.querySelector('#chart')?.append(group);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import './main.css';
-import { xAxis } from './chart/chart';
+import { timeSeriesLine, xAxis, yAxis } from './chart/chart';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="navbar bg-base-100">
     <a class="btn btn-ghost text-xl">Stocks</a>
   </div>
   <div class="hero min-h-screen bg-base-200">
-    <svg width="1400" height="500" id="chart" xmlns="http://www.w3.org/2000/svg">
-    </svg>
+    <svg id="chart" width="1400" height="500"/>
   </div>
 `;
 
 xAxis();
+yAxis();
+timeSeriesLine();
