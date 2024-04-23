9defe839ce2de1528c92cb4198112cdeb597ae08
mr-wellick <lorenz3192@gmail.com>
2024-04-23 01:24:03 -0700

    retrieve stock list and retrieve data for selected stock from list
    	- todo: use svelte query (react-query)
    	- todo: figure out how to incorporate api types in app
    	- todo: figure out how to share stock data to chart components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,42 @@
 <script lang="ts">
-	let tickers: any[] = [];
+	let tickers = [];
+	let tickerData = [];
 
 	async function searchTickers(event: KeyboardEvent) {
 		const { value } = event.target as HTMLInputElement;
-		const res = await fetch(
-			`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${value}&apikey=${
-				import.meta.env.ALPHA_VANTAGE_API_KEY
-			}`
-		);
+		const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${value}&apikey=${
+			import.meta.env.ALPHA_VANTAGE_API_KEY
+		}`;
+
+		//const demoUrl =
+		//	'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=tesco&apikey=demo';
+		const res = await fetch(url);
 
 		try {
-			tickers = await res.json();
-			tickers = tickers.bestMatches;
+			tickers = (await res.json()).bestMatches;
 		} catch (error) {
 			console.error('Failed to fetch ticker results', error);
 			tickers = [];
 		}
-		console.log(tickers);
+	}
+	async function getStock(event: MouseEvent) {
+		const { dataset } = event.target as HTMLButtonElement;
+
+		const res = await fetch(
+			`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${dataset.stock}&apikey=${import.meta.env.ALPHA_VANTAGE_API_KEY}`
+		);
+
+		try {
+			tickerData = await res.json();
+
+			tickerData = Object.keys(tickerData['Time Series (Daily)']).map((date) => ({
+				x: new Date(date),
+				y: +tickerData['Time Series (Daily)'][date]['4. close']
+			}));
+		} catch (error) {
+			console.error(error);
+			return [];
+		}
 	}
 </script>
 
@@ -25,11 +45,13 @@
 </div>
 
 <div class="stats shadow bg-base-200 flex m-5">
-	<form method="POST">
-		<label for="ticker">
+	<div class="min-w-64">
+		<label class="input input-bordered flex items-center gap-2" for="ticker">
 			<input
-				on:keyup={searchTickers}
 				type="text"
+				class="grow"
+				placeholder="Search"
+				on:keyup={searchTickers}
 				id="ticker"
 				name="ticker"
 				required
@@ -37,14 +59,29 @@
 				maxlength="8"
 				size="10"
 			/>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 16 16"
+				fill="currentColor"
+				class="w-4 h-4 opacity-70"
+				><path
+					fill-rule="evenodd"
+					d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+					clip-rule="evenodd"
+				/></svg
+			>
 		</label>
 
-		<uL>
-			{#each tickers as ticker}
-				<li>{ticker['2. name']}</li>
-			{/each}
-		</uL>
-	</form>
+		{#if tickers.length}
+			<ul class="menu rounded-box min-w-64 flex">
+				{#each tickers as ticker}
+					<button class="cursor-pointer" on:click={getStock} data-stock={ticker['1. symbol']}>
+						{ticker['2. name']}
+					</button>
+				{/each}
+			</ul>
+		{/if}
+	</div>
 
 	<div class="stat">
 		<div class="stat-title">IBM</div>
